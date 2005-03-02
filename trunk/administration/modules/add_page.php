ebb8868402e8118c91f9ba232fc38ee21641cfdc
lark <lark@68905588-48f1-0310-a723-09cfc018eb8a>
2005-03-02 14:48:03 +0000

    1.Usunięcie zbędnych instancji klasy 2.Poprawa czytelności zapytań

--- trunk/administration/modules/add_page.php
+++ trunk/administration/modules/add_page.php
@@ -8,44 +8,59 @@ if($action == "add") {
 	
 	$text = nl2br($_POST['text']);
 	
-	$title 			= $_POST['title'];
-	$published 		= $_POST['published'];
+	$title 		= $_POST['title'];
+	$published 	= $_POST['published'];
 	
-	$data_base = new MySQL_DB;
-	$data_base->query("INSERT INTO $mysql_data[db_table_pages] VALUES ('', '$title', '$text', '', '$published')");
-	$data_base->next_record();
+	$db = new MySQL_DB;
 	
-	$d_b = new MySQL_DB;
- 	$d_b->query("SELECT max(id) as maxid FROM $mysql_data[db_table_pages]");
- 	$d_b->next_record();
+	$query = "	INSERT INTO 
+					$mysql_data[db_table_pages] 
+				VALUES 
+					('', '$title', '$text', '', '$published')";
+	
+	$db->query($query);
+	$db->next_record();
+	
+	
+	$query = "	SELECT MAX(id) 
+					as maxid 
+				FROM 
+					$mysql_data[db_table_pages]";
+	
+ 	$db->query($query);
+ 	$db->next_record();
 		
 	// Przypisanie zmiennej $id
-	$id = $d_b->f("0");
+	$id = $db->f("0");
 	
 	if(!empty($_FILES['file']['name'])) {
 		
 		$up = new upload;
 		$upload_dir = "../photos";
 		
-		// use function to upload file.
+		// upload file.
 		$file = $up->upload_file($upload_dir, 'file', true, true, 0, "jpg|jpeg|gif");
 		if($file == false) {
 			
 			echo $up->error;
 		} else {
 		
- 			$d_base = new MySQL_DB;
- 			$d_base->query("UPDATE $mysql_data[db_table_pages] 
- 							SET image='$file' 
- 							WHERE (id='$id')");
-			$d_base->next_record();
+			$query = "	UPDATE 
+							$mysql_data[db_table_pages] 
+						SET 
+							image = '$file' 
+						WHERE 
+							id = '$id'";
+			
+			$db->query($query);
+			$db->next_record();
 			
 			$ft->assign('CONFIRM', "Zdjêcie zosta³o dodane.<br />");
 			$ft->parse('ROWS',	".result_note");
 		}
 	}
 	
-	$ft->assign('CONFIRM', "Wpis zosta³ dodany");
+	$ft->assign('CONFIRM', "Strona zosta³a dodana");
 	$ft->parse('ROWS',	".result_note");
 	
 } else {
