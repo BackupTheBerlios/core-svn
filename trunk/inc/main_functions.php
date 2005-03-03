a8acc0079232cc48559828e3ef9270ad677f0880
mysz <mysz@68905588-48f1-0310-a723-09cfc018eb8a>
2005-03-03 01:42:04 +0000

    optymalizacja coreDateConvert() - serie if'ow zastapiona tablica asocjacyjna

--- trunk/inc/main_functions.php
+++ trunk/inc/main_functions.php
@@ -32,18 +32,21 @@ function coreDateConvert($date) {
 	$newdate	= explode(' ', $date);
 	
 	$date_ex	= explode('-', $newdate[0]);
-	if($date_ex[1] == '01') $date_ex[1] = "Stycznia";
-	if($date_ex[1] == '02') $date_ex[1] = "Lutego";
-	if($date_ex[1] == '03') $date_ex[1] = "Marca";
-	if($date_ex[1] == '04') $date_ex[1] = "Kwietnia";
-	if($date_ex[1] == '05') $date_ex[1] = "Maja";
-	if($date_ex[1] == '06') $date_ex[1] = "Czerwca";
-	if($date_ex[1] == '07') $date_ex[1] = "Lipca";
-	if($date_ex[1] == '08') $date_ex[1] = "Sierpnia";
-	if($date_ex[1] == '09') $date_ex[1] = "Wrze¶nia";
-	if($date_ex[1] == '10') $date_ex[1] = "Pa¼dziernika";
-	if($date_ex[1] == '11') $date_ex[1] = "Listopada";
-	if($date_ex[1] == '12') $date_ex[1] = "Grudnia";
+	$months = array(
+		'01' => 'Stycznia',
+		'02' => 'Lutego',
+		'03' => 'Marca',
+		'04' => 'Kwietnia',
+		'05' => 'Maja',
+		'06' => 'Czerwca',
+		'07' => 'Lipca',
+		'08' => 'Sierpnia',
+		'09' => 'Wrze¶nia',
+		'10' => 'Pa¼dziernika',
+		'11' => 'Listopada',
+		'12' => 'Grudnia'
+	)
+	$date_ex[1] = $months[$date_ex[1]];
 	
 	$date		= $date_ex[2] . " " . $date_ex[1] . ", " . $date_ex[0] . "&nbsp;&nbsp;" . $newdate[1];
 	
