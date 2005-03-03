905946a02ce89a40d51846c33f6890ad88124985
mysz <mysz@68905588-48f1-0310-a723-09cfc018eb8a>
2005-03-03 01:31:05 +0000

    optymalizacja sql query, wywalenie zbednych instancji klasy MySQL_DB

--- trunk/counter/addhit.php
+++ trunk/counter/addhit.php
@@ -1,7 +1,6 @@
 <?php
 
-$Cookie_length = time()+10800;
-$expires = $Cookie_length;
+$expires = time()+10800;
 
 $data_base = new MySQL_DB;
 
@@ -19,16 +18,10 @@ if(!isset($_COOKIE['devlog_counter'])){
 	
 	setcookie($name, $value, $expires);
 	
-	$data_base = new MySQL_DB;
-	$hitnumber = $hitnumber+1;
-	
 	$data_base->query("	UPDATE $mysql_data[db_table_counter] 
-						SET hit = '$value', hitnumber = '$hitnumber+1'");
+						SET hit = '$value', hitnumber = hitnumber+1");
 	$data_base->next_record();
 	
-}else{
-	
-	//do nothing
 }
 
 ?>
