7a3af108746ec0f8f7f58871544e801f8b29a255
lark <lark@68905588-48f1-0310-a723-09cfc018eb8a>
2005-03-03 14:25:26 +0000

    Kategoria podrzędna strony wyświetlana na stronie głównej. Definicja wcięcia jeśli jest dzieckiem(child)

--- trunk/modules/pages_list.php
+++ trunk/modules/pages_list.php
@@ -1,10 +1,19 @@
 <?php
 
 $sql = new MySQL_DB;
-$sql->query("	SELECT id, title 
-				FROM $mysql_data[db_table_pages] 
-				WHERE published = 'Y' 
-				ORDER BY id ASC");
+$query = "	SELECT 
+				id, parent_id, title 
+			FROM 
+				$mysql_data[db_table_pages] 
+			WHERE 
+				parent_id = '0' 
+			AND	
+				published = 'Y' 
+			ORDER BY 
+				id 
+			ASC";
+
+$sql->query($query);
 
 $ft->define(array(	'pages_header'	=>"pages_header.tpl",
 					'pages_list'	=>"pages_list.tpl"));
@@ -14,13 +23,44 @@ if($sql->num_rows() !== 0) {
 	while($sql->next_record()) {
 	
 		$page_id 	= $sql->f("id");
+		$parent_id 	= $sql->f("parent_id");
 		$page_name 	= $sql->f("title");
 	
 		$ft->assign(array(	'PAGE_NAME'		=>$page_name,
-							'PAGE_ID'		=>$page_id));
+							'PAGE_ID'		=>$page_id,
+							'PARENT'		=>''));
 
 				
 		$ft->parse('PAGES_LIST', ".pages_list");
+		
+		$query = "	SELECT 
+						id, parent_id, title 
+					FROM 
+						$mysql_data[db_table_pages] 
+					WHERE 
+						parent_id = '$page_id' 
+					AND 
+						published = 'Y' 
+					ORDER BY 
+						id 
+					ASC";
+
+		$db = new MySQL_DB;
+		$db->query($query);
+		
+		while($db->next_record()) {
+	
+			$page_id 	= $db->f("id");
+			$parent_id 	= $db->f("parent_id");
+			$page_name 	= $db->f("title");
+	
+			$ft->assign(array(	'PAGE_NAME'		=>$page_name,
+								'PAGE_ID'		=>$page_id,
+								'PARENT'		=>'&nbsp; &nbsp;- '));
+
+				
+			$ft->parse('PAGES_LIST', ".pages_list");
+		}
 	}
 
 	$ft->parse('PAGES_HEADER', ".pages_header");
