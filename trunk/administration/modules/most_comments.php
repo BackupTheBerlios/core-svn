06a3fd38c2ddb8561f3f6e4a12b8e4a47e2f9f12
mysz <mysz@68905588-48f1-0310-a723-09cfc018eb8a>
2005-03-05 01:44:31 +0000

    optymalizacja, czyszczenie kodu przejscie z if() na switch()

--- trunk/administration/modules/most_comments.php
+++ trunk/administration/modules/most_comments.php
@@ -5,151 +5,142 @@ $action = empty($_GET['action']) ? '' : $_GET['action'];
 
 $db = new MySQL_DB;
 
-if(empty($action)) {
-	
-	$query = " 	SELECT 
-					n.id, n.title, n.date, 
-					count(DISTINCT c.id) 
-				AS 
-					comments 
-				FROM 
-					$mysql_data[db_table] n 
-				LEFT JOIN 
-					$mysql_data[db_table_comments] c 
-				ON 
-					n.id = c.comments_id 
-				GROUP BY 
-					n.id 
-				HAVING 
-					count(c.id) > 0 	
-				ORDER BY 
-					comments 
-				DESC 
-				LIMIT 
-					20";
-	
-	$db->query($query);
-	
-	// Sprawdzamy, czy w bazie danych s± ju¿ jakie¶ wpisy
-	if($db->num_rows() !== 0) {
-	
-		// Pêtla wyswietlaj¹ca wszystkie wpisy + stronnicowanie ich
-		while($db->next_record()) {
-	
-			$id 		= $db->f("id");
-			$title 		= $db->f("title");
-			$date 		= $db->f("date");
-			$comments 	= $db->f("comments");
+switch ($action)
+{
+	case "show":// wy¶wietlanie wpisu pobranego do modyfikacji
+		$query = "	SELECT * 
+					FROM 
+						$mysql_data[db_table_comments] 
+					WHERE 
+						id = '$_GET[id]'";
 		
-			$date = explode(' ', $date);
+		$db->query($query);
+		$db->next_record();
 		
-			$ft->assign(array(	'ID'		=>$id,
-								'TITLE'		=>$title,
-								'DATE'		=>$date[0],
-								'COMMENTS'	=>$comments));				
+		$date 		= $db->f("date");
+		$title 		= $db->f("title");
+		$text 		= $db->f("text");
+		$author		= $db->f("author");
+		$published	= $db->f("published");
+
 		
-			// deklaracja zmiennej $idx1::color switcher
-			$idx1 = empty($idx1) ? '' : $idx1;
-			
-			$idx1++;
+		$date	= substr($date, 0, 16);
+		$dat1	= explode(" ", $date);
+		$dat	= explode("-", $dat1[0]);
+		$date	= "$dat[2]-$dat[1]-$dat[0] <b>$dat1[1]</b>";
 		
-			// naprzemienne kolorowanie wierszy tabeli
-			if (($idx1%2)==1) {
-			
-				$ft->assign('ID_CLASS', "id=\"mainList\"");
-				// parsowanie szablonów
-				$ft->parse('NOTE_ROWS',	".table_mostcommentslist");
-			} else {
-			
-				$ft->assign('ID_CLASS', "id=\"mainListAlter\"");
-				$ft->parse('NOTE_ROWS',	".table_mostcommentslist");
-			}
-		}
-	
-		$ft->parse('ROWS',	".header_mostcommentslist");
-	} else {
-	
-		$ft->assign('CONFIRM', "W bazie danych nie ma ¿adnych wpisów");
+		$text = str_replace("<br />", "\r\n", $text);
+		$text = preg_replace("/(\r\n)+/g", "\1\1", $text);
+
+		
+		$ft->assign(array(	'AUTHOR'	=>$author,
+							'DATE' 		=>$date,
+							'ID'		=>$_GET['id'],
+							'TEXT'		=>$text));
+
+		$ft->parse('ROWS',	".form_commentsedit");
+		break;
+
+	case "edit":// edycja wybranego wpisu
+		$text		= nl2br($_POST['text']);
+		$author		= $_POST['author'];
+		
+		$query = "	UPDATE 
+						$mysql_data[db_table_comments] 
+					SET 
+						author = '$author', 
+						text = '$text' 
+					WHERE 
+						id = '$_GET[id]'";
+		
+		$db->query($query);
+		
+		$ft->assign('CONFIRM', "Komentarz zosta³ zmodyfikowany.");
 
 		$ft->parse('ROWS',	".result_note");
-	}
+		break;
+
+	case "delete":// usuwanie wybranego wpisu
+		$query = "	DELETE 
+					FROM 
+						$mysql_data[db_table_comments] 
+					WHERE 
+						id = '$_GET[id]'";
+		
+		$db->query($query);
+		
+		$ft->assign('CONFIRM', "Komentarz zosta³ usuniêty.");
+
+		$ft->parse('ROWS', ".result_note");
+		break;
+		
+	default:
+		$query = " 	SELECT 
+						n.id, n.title, n.date, 
+						count(DISTINCT c.id) 
+					AS 
+						comments 
+					FROM 
+						$mysql_data[db_table] n 
+					LEFT JOIN 
+						$mysql_data[db_table_comments] c 
+					ON 
+						n.id = c.comments_id 
+					GROUP BY 
+						n.id 
+					HAVING 
+						count(c.id) > 0 	
+					ORDER BY 
+						comments 
+					DESC 
+					LIMIT 
+						20";
+		
+		$db->query($query);
+		
+		// Sprawdzamy, czy w bazie danych s± ju¿ jakie¶ wpisy
+		if($db->num_rows() > 0) {
+		
+			// Pêtla wyswietlaj¹ca wszystkie wpisy + stronnicowanie ich
+			while($db->next_record()) {
+		
+				$id 		= $db->f("id");
+				$title 		= $db->f("title");
+				$date 		= $db->f("date");
+				$comments 	= $db->f("comments");
+			
+				$date = explode(' ', $date);
+			
+				$ft->assign(array(	'ID'		=>$id,
+									'TITLE'		=>$title,
+									'DATE'		=>$date[0],
+									'COMMENTS'	=>$comments));				
+			
+				// deklaracja zmiennej $idx1::color switcher
+				$idx1 = empty($idx1) ? '' : $idx1;
+				
+				$idx1++;
+			
+				// naprzemienne kolorowanie wierszy tabeli
+				if (($idx1%2)==1) {
+				
+					$ft->assign('ID_CLASS', "id=\"mainList\"");
+				} else {
+				
+					$ft->assign('ID_CLASS', "id=\"mainListAlter\"");
+				}
+
+				// parsowanie szablonów
+				$ft->parse('NOTE_ROWS',	".table_mostcommentslist");
+			}
+		
+			$ft->parse('ROWS',	".header_mostcommentslist");
+		} else {
+		
+			$ft->assign('CONFIRM', "W bazie danych nie ma ¿adnych wpisów");
+
+			$ft->parse('ROWS',	".result_note");
+		}
 }
 
-// wy¶wietlanie wpisu pobranego do modyfikacji
-if ($action == "show") {
-	
-	$query = "	SELECT * 
-				FROM 
-					$mysql_data[db_table_comments] 
-				WHERE 
-					id = '$_GET[id]'";
-	
-	$db->query($query);
-	$db->next_record();
-	
-	$date 		= $db->f("date");
-	$title 		= $db->f("title");
-	$text 		= $db->f("text");
-	$author		= $db->f("author");
-	$published	= $db->f("published");
-
-	
-	$date	= substr($date, 0, 16);
-	$dat1	= explode(" ", $date);
-	$dat	= explode("-", $dat1[0]);
-	$date	= "$dat[2]-$dat[1]-$dat[0] <b>$dat1[1]</b>";
-	
-	$text = str_replace("<br />", "\r\n", $text);
-	$text = preg_replace("/(\r\n)+/g", "\1\1", $text);
-
-	
-	$ft->assign(array(	'AUTHOR'	=>$author,
-						'DATE' 		=>$date,
-						'ID'		=>$_GET['id'],
-						'TEXT'		=>$text));
-
-	$ft->parse('ROWS',	".form_commentsedit");
-	
-}
-
-// edycja wybranego wpisu
-if ($action == "edit") {
-	
-	$text		= nl2br($_POST['text']);
-	$author		= $_POST['author'];
-	
-	$query = "	UPDATE 
-					$mysql_data[db_table_comments] 
-				SET 
-					author = '$author', 
-					text = '$text' 
-				WHERE 
-					id = '$_GET[id]'";
-	
-	$db->query($query);
-	$db->next_record();
-	
-	$ft->assign('CONFIRM', "Komentarz zosta³ zmodyfikowany.");
-
-	$ft->parse('ROWS',	".result_note");
-	
-}
-
-// usuwanie wybranego wpisu
-if ($action == "delete") {
-	
-	$query = "	DELETE 
-				FROM 
-					$mysql_data[db_table_comments] 
-				WHERE 
-					id = '$_GET[id]'";
-	
-	$db->query($query);
-	$db->next_record();
-	
-	$ft->assign('CONFIRM', "Komentarz zosta³ usuniêty.");
-
-	$ft->parse('ROWS', ".result_note");
-	
-}
 ?>
