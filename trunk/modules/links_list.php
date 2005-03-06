2972d05b2a6443f2df5562d56efc196afeed0fb4
lark <lark@68905588-48f1-0310-a723-09cfc018eb8a>
2005-03-06 14:36:44 +0000

    Added a file remotely

---
+++ trunk/modules/links_list.php
@@ -0,0 +1,28 @@
+<?php
+
+$db = new MySQL_DB;
+$db->query("	SELECT 
+					title, url  
+				FROM 
+					$mysql_data[db_table_links] 
+				ORDER BY 
+					id 
+				ASC");
+
+$ft->define('links_list', "links_list.tpl");
+
+	
+while($db->next_record()) {
+	
+	$link_name 	= $db->f("title");
+	$link_url 	= $db->f("url");
+	
+	$link_url	= str_replace('&', '&amp;', $link_url);
+	
+	$ft->assign(array(	'LINK_NAME'	=>$link_name,
+						'LINK_URL'	=>$link_url));
+
+				
+	$ft->parse('LINKS_LIST', ".links_list");
+}
+?>
