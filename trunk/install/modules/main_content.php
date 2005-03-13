f7df7ae234d1b6605db37a23b0a515b378dd2872
lark <lark@68905588-48f1-0310-a723-09cfc018eb8a>
2005-03-13 12:55:55 +0000

    Dodany switch w zależności od akcji. Przygotowanie pod obsługę instalatora

--- trunk/install/modules/main_content.php
+++ trunk/install/modules/main_content.php
@@ -1,7 +1,21 @@
 <?php
 
-$ft->define('main_content', "main_content.tpl");
-$ft->parse('ROWS', ".main_content");
+// deklaracja zmiennej $action::form
+$action = empty($_GET['action']) ? '' : $_GET['action'];
 
+switch ($action) {
+	
+	case "send":
+
+		
+		break;
+
+	default:
+		
+		$ft->assign('PREFIX', 'core_');
+		$ft->define('main_content', "main_content.tpl");
+		$ft->parse('ROWS', ".main_content");
+		break;
+}	
 
 ?>
