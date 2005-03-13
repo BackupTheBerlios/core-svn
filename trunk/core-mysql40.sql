96c81e690ae45336b3a1d5ba01999b7276574c74
lark <lark@68905588-48f1-0310-a723-09cfc018eb8a>
2005-03-13 21:30:36 +0000

    Usunięcie komentarzy z SQL, dodany prefix, który jest podmieniany na dowolny, podany przez użytkownika podczas instalacji. Całość przygotowana pod tworzenie struktury bazy danych bezpośrednio z pliku sql.

--- trunk/core-mysql40.sql
+++ trunk/core-mysql40.sql
@@ -1,22 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 2.6.0-pl3
--- http://www.phpmyadmin.net
--- 
--- Host: localhost
--- Czas wygenerowania: 02 Mar 2005, 02:57
--- Wersja serwera: 4.1.8
--- Wersja PHP: 5.0.3
--- 
--- Baza danych: `devlog`
--- 
-
--- --------------------------------------------------------
-
--- 
--- Struktura tabeli dla  `devlog`
--- 
-
-CREATE TABLE devlog (
+CREATE TABLE core_devlog (
   id mediumint(7) NOT NULL auto_increment,
   c_id int(7) NOT NULL default '1',
   `date` datetime NOT NULL default '0000-00-00 00:00:00',
@@ -26,30 +8,18 @@ CREATE TABLE devlog (
   image varchar(255) NOT NULL default '',
   comments_allow char(1) NOT NULL default '1',
   published enum('Y','N') NOT NULL default 'Y',
-  PRIMARY KEY  (id,c_id)
-);
+  PRIMARY KEY (id,c_id)
+) ENGINE = MyISAM;
 
--- --------------------------------------------------------
-
--- 
--- Struktura tabeli dla  `devlog_category`
--- 
-
-CREATE TABLE devlog_category (
+CREATE TABLE core_category (
   category_id mediumint(7) NOT NULL auto_increment,
   category_name varchar(40) NOT NULL default '',
   category_description text NOT NULL,
   PRIMARY KEY  (category_id),
   KEY id (category_id)
-);
+) ENGINE = MyISAM;
 
--- --------------------------------------------------------
-
--- 
--- Struktura tabeli dla  `devlog_comments`
--- 
-
-CREATE TABLE devlog_comments (
+CREATE TABLE core_comments (
   id mediumint(7) NOT NULL auto_increment,
   `date` datetime NOT NULL default '0000-00-00 00:00:00',
   comments_id varchar(40) NOT NULL default '',
@@ -59,50 +29,26 @@ CREATE TABLE devlog_comments (
   `text` mediumtext NOT NULL,
   PRIMARY KEY  (id,comments_id),
   KEY id (comments_id,id)
-);
+) ENGINE = MyISAM;
 
--- --------------------------------------------------------
-
--- 
--- Struktura tabeli dla  `devlog_config`
--- 
-
-CREATE TABLE devlog_config (
+CREATE TABLE core_config (
   config_name varchar(255) NOT NULL default '',
   config_value varchar(255) NOT NULL default '',
   PRIMARY KEY  (config_name)
-);
+) ENGINE = MyISAM;
 
--- --------------------------------------------------------
-
--- 
--- Struktura tabeli dla  `devlog_counter`
--- 
-
-CREATE TABLE devlog_counter (
+CREATE TABLE core_counter (
   id int(10) unsigned NOT NULL auto_increment,
   hit text,
   hitnumber int(11) default NULL,
   PRIMARY KEY  (id)
-);
+) ENGINE = MyISAM;
 
--- --------------------------------------------------------
-
--- 
--- Struktura tabeli dla  `devlog_newsletter`
--- 
-
-CREATE TABLE devlog_newsletter (
+CREATE TABLE core_newsletter (
   email varchar(40) NOT NULL default ''
-);
+) ENGINE = MyISAM;
 
--- --------------------------------------------------------
-
--- 
--- Struktura tabeli dla  `devlog_pages`
--- 
-
-CREATE TABLE devlog_pages (
+CREATE TABLE core_pages (
   id mediumint(7) NOT NULL auto_increment,
   parent_id mediumint(7) NOT NULL default '0',
   title varchar(50) NOT NULL default '',
@@ -110,47 +56,29 @@ CREATE TABLE devlog_pages (
   image varchar(255) NOT NULL default '',
   published enum('Y','N') NOT NULL default 'Y',
   PRIMARY KEY  (id)
-);
+) ENGINE = MyISAM;
 
--- --------------------------------------------------------
-
--- 
--- Struktura tabeli dla  `devlog_session`
--- 
-
-CREATE TABLE devlog_session (
+CREATE TABLE core_session (
   session_id varchar(32) NOT NULL default '',
   session_ip_address varchar(32) NOT NULL default '',
   session_log_in_time int(10) unsigned NOT NULL default '0',
   session_running_time int(10) unsigned NOT NULL default '0',
   PRIMARY KEY  (session_id)
-);
+) ENGINE = MyISAM;
 
--- --------------------------------------------------------
-
--- 
--- Struktura tabeli dla  `devlog_users`
--- 
-
-CREATE TABLE devlog_users (
+CREATE TABLE core_users (
   id int(5) NOT NULL auto_increment,
   login varchar(15) default '0',
   `password` varchar(32) default '0',
   email varchar(30) NOT NULL default '',
   active enum('Y','N') NOT NULL default 'N',
   PRIMARY KEY  (id)
-);
+) ENGINE = MyISAM;
 
--- --------------------------------------------------------
-
---
--- Struktura tabeli dla  'devlog_links_category'
--- 
-
-CREATE TABLE `devlog_links` (
+CREATE TABLE core_links (
   `id` mediumint(7) NOT NULL auto_increment,
   `title` varchar(40) NOT NULL default '',
   `url` varchar(255) NOT NULL default '',
   PRIMARY KEY  (`id`),
   KEY `id` (`id`)
-);
+) ENGINE = MyISAM;
