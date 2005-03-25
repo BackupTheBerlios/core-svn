d6f5c07328204b5a89c2cbbfba5f75f342642f77
lark <lark@68905588-48f1-0310-a723-09cfc018eb8a>
2005-03-25 11:53:27 +0000

    Dodana funkcja pobierająca rekurencyjnie kategorie i podkategorie stron. Poziom zagłębień teoretycznie nieograniczony, pozostaje dopisać odpowiednio zwiększające się wcięcie, przy każdym kolejnym dziecku.

--- trunk/modules/pages_list.php
+++ trunk/modules/pages_list.php
@@ -18,6 +18,41 @@ $sql->query($query);
 $ft->define(array(	'pages_header'	=>"pages_header.tpl",
 					'pages_list'	=>"pages_list.tpl"));
 					
+function get_cat($page_id, $separator) {
+	
+	global $mysql_data, $ft;
+
+	$query = "	SELECT 
+					id, parent_id, title 
+				FROM 
+					$mysql_data[db_table_pages] 
+				WHERE 
+					parent_id = '$page_id' 
+				AND 
+					published = 'Y' 
+				ORDER BY 
+					id 
+				ASC";
+
+	$db = new MySQL_DB;
+	$db->query($query);
+		
+	while($db->next_record()) {
+	
+		$page_id 	= $db->f("id");
+		$parent_id 	= $db->f("parent_id");
+		$page_name 	= $db->f("title");
+	
+		$ft->assign(array(	'PAGE_NAME'		=>$page_name,
+							'PAGE_ID'		=>$page_id,
+							'PARENT'		=>$separator));
+
+				
+		$ft->parse('PAGES_LIST', ".pages_list");
+		get_cat($page_id, ' &nbsp; &nbsp;- ');
+	}
+}
+					
 if($sql->num_rows() > 0) {
 
 	while($sql->next_record()) {
@@ -29,38 +64,11 @@ if($sql->num_rows() > 0) {
 		$ft->assign(array(	'PAGE_NAME'		=>$page_name,
 							'PAGE_ID'		=>$page_id,
 							'PARENT'		=>''));
-
 				
 		$ft->parse('PAGES_LIST', ".pages_list");
 		
-		$query = "	SELECT 
-						id, parent_id, title 
-					FROM 
-						$mysql_data[db_table_pages] 
-					WHERE 
-						parent_id = '$page_id' 
-					AND 
-						published = 'Y' 
-					ORDER BY 
-						id 
-					ASC";
-
-		$db = new MySQL_DB;
-		$db->query($query);
-		
-		while($db->next_record()) {
-	
-			$page_id 	= $db->f("id");
-			$parent_id 	= $db->f("parent_id");
-			$page_name 	= $db->f("title");
-	
-			$ft->assign(array(	'PAGE_NAME'		=>$page_name,
-								'PAGE_ID'		=>$page_id,
-								'PARENT'		=>'&nbsp; &nbsp;- '));
-
-				
-			$ft->parse('PAGES_LIST', ".pages_list");
-		}
+		// funkcja pobieraj±ca rekurencyjnie kategorie
+		get_cat($page_id, ' &nbsp; &nbsp;- ');
 	}
 
 	$ft->parse('PAGES_HEADER', ".pages_header");
