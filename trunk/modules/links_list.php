bf660ffce843ab71c9e02aec46fc6e31d46290f3
lark <lark@68905588-48f1-0310-a723-09cfc018eb8a>
2005-03-29 11:52:40 +0000

    Skrocenie kodu odpowiedzialnego za zamiane & -> &amp; w linkach

--- trunk/modules/links_list.php
+++ trunk/modules/links_list.php
@@ -15,9 +15,7 @@ $ft->define('links_list', "links_list.tpl");
 while($db->next_record()) {
 	
 	$link_name 	= $db->f("title");
-	$link_url 	= $db->f("url");
-	
-	$link_url	= str_replace('&', '&amp;', $link_url);
+	$link_url	= str_replace('&', '&amp;', $db->f("url"));
 	
 	$ft->assign(array(	'LINK_NAME'	=>$link_name,
 						'LINK_URL'	=>$link_url));
