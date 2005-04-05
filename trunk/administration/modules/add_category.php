087e17d405fb08cff74e6d2709bcc5e14e5a1376
lark <lark@68905588-48f1-0310-a723-09cfc018eb8a>
2005-04-05 19:18:16 +0000

    Ujednolicenie zapytan. Dodanie wyrazen regularnych sprawdzajacych, czy nazwa dodawanej kategorii nie jest pusta.

--- trunk/administration/modules/add_category.php
+++ trunk/administration/modules/add_category.php
@@ -3,28 +3,52 @@
 // deklaracja zmiennej $action::form
 $action = empty($_GET['action']) ? '' : $_GET['action'];
 
-switch ($action)
-{
+switch ($action) {
+	
 	case "add":
+	
 		$category_name			= $_POST['category_name'];
 		$category_description	= $_POST['category_description'];
+		
+		$monit = empty($monit) ? '' : $monit;
+	
+		// Obs³uga formularza, jesli go zatwierdzono
+		if(!eregi("^([^0-9]+){2,}$", $category_name)) {
 			
-		// egzemplarz klasy ³aduj±cej komentarz do bazy danych
-		$db = new MySQL_DB;
+			$monit .= "Musisz podaæ nazwê kategorii.<br />";
+		}
+		
+		if(empty($monit)) {
 			
-		$db->query("INSERT INTO $mysql_data[db_table_category] VALUES ('', '$category_name', '$category_description')");
+			// egzemplarz klasy ³aduj±cej komentarz do bazy danych
+			$db = new MySQL_DB;
 			
-		$ft->assign('CONFIRM', "Kategoria zosta³a dodana do bazy danych");
+			$query = "	INSERT INTO 
+							$mysql_data[db_table_category] 
+						VALUES 
+							('', '$category_name', '$category_description')";
+		
+			$db->query($query);
+			
+			$ft->assign('CONFIRM', "Kategoria zosta³a dodana do bazy danych");
+		} else {
+			
+			$monit .= "<br /><a href=\"javascript:history.back(-1);\">powrót</a>";
+			$ft->assign('CONFIRM', $monit);
+		}
 		$ft->parse('ROWS', ".result_note");
 		break;
 
 	default:
+	
 		// przydzielenie zmiennych::array
-		$ft->assign(array(	'SUBMIT_URL'		=>"add,8,action.html",
-							'CATNAME_VALUE'		=>"",
-							'CATNAME_DESC'		=>"",
-							'SUBMIT_HREF_DESC'	=>"dodaj now± kategoriê",
-							'HEADER_DESC'		=>"<b>Kategorie - dodaj now± kategoriê</b>"));
+		$ft->assign(array(
+						'SUBMIT_URL'		=>"add,8,action.html",
+						'CATNAME_VALUE'		=>"",
+						'CATNAME_DESC'		=>"",
+						'SUBMIT_HREF_DESC'	=>"dodaj now± kategoriê",
+						'HEADER_DESC'		=>"<b>Kategorie - dodaj now± kategoriê</b>"
+		));
 		
 		// w przypadku braku akcji wy¶wietlanie formularza
 		$ft->define('form_category', "form_category.tpl");
