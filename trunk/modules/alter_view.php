591972862cf59753781d88209221f74a7cc734ce
mysz <mysz@68905588-48f1-0310-a723-09cfc018eb8a>
2005-04-05 23:09:40 +0000

    kilka zmian dot. uwag 4lark

--- trunk/modules/alter_view.php
+++ trunk/modules/alter_view.php
@@ -2,28 +2,37 @@
 
 $db = new MySQL_DB;
 
-$query = sprintf("	
-			SELECT 
-				a.*, b.*, c.comments_id, count(c.id) 
-			AS 
-				comments 
-			FROM 
-				$mysql_data[db_table] a, $mysql_data[db_table_category] b 
-			LEFT JOIN 
-				$mysql_data[db_table_comments] c 
-			ON 
-				a.id = c.comments_id
-			WHERE 
-				a.id = '%1\$d' 
-			AND 
-				b.category_id = a.c_id 
-			AND 
-				published = 'Y' 
-			GROUP BY 
-				a.date 
-			DESC 
-			LIMIT 
-				1", $_GET['id']);
+//skoro uzywasz juz printfa, to daj mu popracowac...
+//no i ladnie uloz cale zapytanie, wtedy bedzie czytelniejsze
+$query = sprintf("
+  SELECT 
+    a.*,
+    b.*,
+    c.comments_id,
+    count(c.id) AS comments 
+  FROM 
+    %s a,
+    %s b 
+  LEFT JOIN 
+      %s c 
+    ON 
+      a.id = c.comments_id
+  WHERE 
+    a.id = '%d' 
+  AND 
+    b.category_id = a.c_id 
+  AND 
+    published = 'Y' 
+  GROUP BY 
+    a.date 
+  DESC 
+  LIMIT 1",
+
+  $mysql_data['db_table'],
+  $mysql_data['db_table_category'],
+  $mysql_data['db_table_comments'],
+  $_GET['id']
+);
 
 $db->query($query);
 
@@ -42,37 +51,41 @@ if($db->num_rows() > 0) {
 	
 	$c_id 			= $db->f("category_id");
 	
+  //do tego przydalaby sie konkretna funkcja - jesli cos wykonujesz 2 razy w ten sam sposob, stworz temu funkcje. przyspieszy to wykonanie.
 	$c_name	= str_replace('&', '&amp;', $db->f("category_name"));
 		
-	// Przypisanie zmiennej $comments
 	$comments 		= $db->f("comments");
 
-	// konwersja daty na bardziej ludzki format
 	$date			= coreDateConvert($date);
 	
-	$ft->assign(array(	'DATE'				=>$date,
-						'NEWS_TITLE'		=>$title,
-						'NEWS_TEXT'			=>$text,
-						'NEWS_AUTHOR'		=>$author,
-						'NEWS_ID'			=>$id,
-						'CATEGORY_NAME'		=>$c_name,
-						'NEWS_CATEGORY'		=>$c_id,
-						'STRING'			=>""));
+  //to tez landie mozna poukladac - zwiekszysz czytelnosc kodu. php umozliwia zrobienie z kodu smietnika, ale pozwala Ci tez na ulozenie go...
+  $ft->assign(array(
+    'DATE'				=>$date,
+		'NEWS_TITLE'		=>$title,
+		'NEWS_TEXT'			=>$text,
+		'NEWS_AUTHOR'		=>$author,
+		'NEWS_ID'			=>$id,
+		'CATEGORY_NAME'		=>$c_name,
+		'NEWS_CATEGORY'		=>$c_id,
+    'STRING'			=>""
+  ));
 
-	if(($comments_allow) == 0 ) {
+  //czytelnosc: dla oka i mozgu latwiej jest zrozumiec zapis:
+  if(! $comments_allow) {
+  //niz :
+	//if(($comments_allow) == 0 ) {
 			
 		$ft->assign(array('COMMENTS_ALLOW'	=>"<br />"));
 	} else {
-		
+
+    //tu ew. tez mozna uzyc wersji z ! - w php 0 jest tozsame (dla porownania '==') z false
 		if($comments == 0) {
 			
-			// template prepare
 			$ft->define('comments_link_empty', "comments_link_empty.tpl");
 				
 			$ft->parse('COMMENTS_ALLOW', "comments_link_empty");
 		} else {
 			
-			// template prepare
 			$ft->define('comments_link_alter', "comments_link_alter.tpl");
 			$ft->assign('COMMENTS', $comments);
 				
@@ -84,14 +97,21 @@ if($db->num_rows() > 0) {
 
 		$ft->assign(array('IMAGE' =>""));
 	} else {
+
+    //w main_functions.php masz funkcje dodane preze mnie, w tym get_root(). tam, gdzie korzystasz z takich sciezek, warto korzystac z tejze funkcji - zwraca ona katalog domowy core.
+    //ponizej 2x korzystasz z tworzeni=a sciezki do obrazka - w if'ie, i przy pobieraniu wymiarow. wstaw ta sciezke do zmiennej:
+    $img_path = get_root() . '/photos/' . $image;
+  //tak btw - parsowanie tekstu w '' jest szybsze niz w "" - robilem swego czasu testy. wszystko, czego nie potrzebujesz parsowac, jak np ponizej w linii 118, warto wrzucac w '' zamiast w "". a poza tym, staraj sie zawsze unikac wsadzania tekstu i zmiennych w "", bo raz, ze obniza to czytelnosc, dwa, ze powoduje problemy z bezpieczenstwem czasem - [s]printf pozwala uniknac atakow html i sql injection
+		if(is_file($img_path)) {
+			list($width, $height) = getimagesize($img_path);
 		
-		if(is_file('photos/' . $image)) {
-			list($width, $height) = getimagesize("photos/" . $image);
+      //czytelnosc - takie ulozenie nawiasow pozwala na latwiejsze wychwycenie blokow kodu
+      $ft->assign(array(
+        'WIDTH'		=>$width,
+        'HEIGHT'	=>$height
+      ));
 		
-			// wysoko¶æ, szeroko¶æ obrazka
-			$ft->assign(array(	'WIDTH'		=>$width,
-								'HEIGHT'	=>$height));
-		
+      //takie cos nie powinno byc na stale, tylko gdzies konfigurowalne. mamy zakladke settings w core ? a jesli nie chcesz tam, to w config.php utworz stala ktora bedzie sie tam modyfikowac, a ktora w tym przypadku bedzie zawierac 440
 			if($width > 440) {
 			
 				// template prepare
@@ -113,10 +133,16 @@ if($db->num_rows() > 0) {
 	$ft->parse('ROWS',".single_rows");
 } else {
 	
-	$ft->assign(array(	'QUERY_FAILED'	=>"W bazie danych nie ma wpisu o ¿±danym id",
-						'STRING'			=>""));
+  //czytelnosc. zebys wiedzial o czym mowie, zostawiam Twoja wersje zakomentowana
+	//$ft->assign(array(	'QUERY_FAILED'	=>"W bazie danych nie ma wpisu o ¿±danym id",
+	//					'STRING'			=>""));
+  $ft->assign(array(
+    'QUERY_FAILED'	=>"W bazie danych nie ma wpisu o ¿±danym id",
+		'STRING'			=>""
+  ));
 	
 	$ft->parse('ROWS',".query_failed");
 }
 
+//ps. umawialismy sie na to, zeby zmienic wciecia i uzywac spacji zamiast tabow... abstrachuje tu od ilosci tychze soacji, dla mnie najwygodniejsze jest 2, ale to mozna sie dogadac jeszcze.. :)
 ?>
