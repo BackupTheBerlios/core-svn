44918534933ff92f2e94907a0d15b619a261c9ef
lark <lark@68905588-48f1-0310-a723-09cfc018eb8a>
2005-04-05 19:12:50 +0000

    Ujednolicenie zapytan, dodane wyrazenia regularne sprawdzajace, czy nazwa linku, jak i URI ma poprawny format.

--- trunk/administration/modules/add_links.php
+++ trunk/administration/modules/add_links.php
@@ -3,34 +3,62 @@
 // deklaracja zmiennej $action::form
 $action = empty($_GET['action']) ? '' : $_GET['action'];
 
-switch ($action)
-{
+switch ($action) {
 	case "add":
+	
 		$link_name	= $_POST['link_name'];
 		$link_url	= $_POST['link_url'];
 
-		if (substr($link_url, 0, 7) != 'http://' && substr($link_url, 0, 6) != 'ftp://' && substr($link_url, 0, 8) != 'https://')
-		{
+		if(	substr($link_url, 0, 7) != 'http://' && 
+			substr($link_url, 0, 6) != 'ftp://' && 
+			substr($link_url, 0, 8) != 'https://') {
+				
 			$link_url = 'http://' . $link_url;
 		}
+		
+		$monit = empty($monit) ? '' : $monit;
+	
+		// Obs³uga formularza, jesli go zatwierdzono
+		if(!eregi("^([^0-9]+){2,}$", $link_name)) {
 			
-		// egzemplarz klasy ³aduj±cej komentarz do bazy danych
-		$db = new MySQL_DB;
+			$monit .= "Musisz podaæ nazwê linku.<br />";
+		}
+		
+		if(!eregi("^(www|ftp|http)://([-a-z0-9]+\.)+([a-z]{2,})$", $link_url)) {
 			
-		$query = "INSERT INTO $mysql_data[db_table_links] VALUES ('', '$link_name', '$link_url')";
-		$db->query($query);
+			$monit .= "Link musi byæ w poprawnym formacie (www|ftp|http)://example.com<br />";
+		}
+		
+		if(empty($monit)) {
 			
-		$ft->assign('CONFIRM', "Link zosta³ dodany do bazy danych");
+			// egzemplarz klasy ³aduj±cej komentarz do bazy danych
+			$db = new MySQL_DB;
+			
+			$query = "	INSERT INTO 
+							$mysql_data[db_table_links] 
+						VALUES 
+							('', '$link_name', '$link_url')";
+		
+			$db->query($query);
+			
+			$ft->assign('CONFIRM', "Link zosta³ dodany do bazy danych");
+		} else {
+			
+			$monit .= "<br /><a href=\"javascript:history.back(-1);\">powrót</a>";
+			$ft->assign('CONFIRM', $monit);
+		}
 		$ft->parse('ROWS', ".result_note");
 		break;
 
 	default:
 		// przydzielenie zmiennych::array
-		$ft->assign(array(	'SUBMIT_URL'		=>"add,11,action.html",
-							'LINK_VALUE'		=>"",
-							'LINKURL_VALUE'		=>"value=\"http://\"",
-							'SUBMIT_HREF_DESC'	=>"dodaj nowy link",
-							'HEADER_DESC'		=>"<b>Linki - dodaj nowy link</b>"));
+		$ft->assign(array(
+						'SUBMIT_URL'		=>"add,11,action.html",
+						'LINK_VALUE'		=>"",
+						'LINKURL_VALUE'		=>"value=\"http://\"",
+						'SUBMIT_HREF_DESC'	=>"dodaj nowy link",
+						'HEADER_DESC'		=>"<b>Linki - dodaj nowy link</b>"
+		));
 		
 		// w przypadku braku akcji wy¶wietlanie formularza
 		$ft->define('form_linkadd', "form_linkadd.tpl");
--- trunk/administration/modules/edit_links.php
+++ trunk/administration/modules/edit_links.php
@@ -5,30 +5,31 @@ $action = empty($_GET['action']) ? '' : $_GET['action'];
 
 $db = new MySQL_DB;
 
-switch ($action)
-{
+switch ($action) {
 	case "show": // wy¶wietlanie wpisu pobranego do modyfikacji
-		$query = "SELECT * 
-					FROM 
+	
+		$query = "	SELECT * FROM 
 						$mysql_data[db_table_links] 
 					WHERE 
 						id = '$_GET[id]'";
-		$db->query($query);
 		
+		$db->query($query);
 		$db->next_record();
 		
 		$link_id	= $db->f("id");
 		$link_name	= $db->f("title");
 		$link_url	= $db->f("url");
 		
-		$ft->assign(array(	'LINK_ID'			=>$link_id,
-							'LINK_NAME'			=>$link_name,
-							'LINK_URL'			=>$link_url,
-							'SUBMIT_URL'		=>"edit," . $link_id . ",12,edit.html",
-							'LINK_VALUE'		=>"value=\"" . $link_name . "\"",
-							'LINKURL_VALUE'		=>"value=\"" . $link_url . "\"",
-							'SUBMIT_HREF_DESC'	=>"zmodyfikuj link",
-							'HEADER_DESC'		=>"<b>Linki - modyfikacja linku</b>"));
+		$ft->assign(array(	
+						'LINK_ID'			=>$link_id,
+						'LINK_NAME'			=>$link_name,
+						'LINK_URL'			=>$link_url,
+						'SUBMIT_URL'		=>"edit," . $link_id . ",12,edit.html",
+						'LINK_VALUE'		=>"value=\"" . $link_name . "\"",
+						'LINKURL_VALUE'		=>"value=\"" . $link_url . "\"",
+						'SUBMIT_HREF_DESC'	=>"zmodyfikuj link",
+						'HEADER_DESC'		=>"<b>Linki - modyfikacja linku</b>"
+		));
 
 		$ft->define('form_linkadd', "form_linkadd.tpl");
 		$ft->parse('ROWS',	".form_linkadd");
@@ -38,21 +39,44 @@ switch ($action)
 		$link_name	= $_POST['link_name'];
 		$link_url	= $_POST['link_url'];
 
-		if (substr($link_url, 0, 7) != 'http://' && substr($link_url, 0, 6) != 'ftp://' && substr($link_url, 0, 8) != 'https://')
-		{
+		if(	substr($link_url, 0, 7) != 'http://' && 
+			substr($link_url, 0, 6) != 'ftp://' && 
+			substr($link_url, 0, 8) != 'https://') {
+				
 			$link_url = 'http://' . $link_url;
 		}
 		
-		$query = "UPDATE 
-						$mysql_data[db_table_links] 
-					SET 
-						title	= '$link_name', 
-						url		= '$link_url' 
-					WHERE 
-						id = '$_GET[id]'";
-		$db->query($query);
+		$monit = empty($monit) ? '' : $monit;
+	
+		// Obs³uga formularza, jesli go zatwierdzono
+		if(!eregi("^([^0-9]+){2,}$", $link_name)) {
+			
+			$monit .= "Musisz podaæ nazwê linku.<br />";
+		}
 		
-		$ft->assign(array(	'CONFIRM'	=>"Link zosta³ zmodyfikowany."));
+		if(!eregi("^(www|ftp|http)://([-a-z0-9]+\.)+([a-z]{2,})$", $link_url)) {
+			
+			$monit .= "Link musi byæ w poprawnym formacie (www|ftp|http)://example.com<br />";
+		}
+		
+		if(empty($monit)) {
+		
+			$query = "	UPDATE 
+							$mysql_data[db_table_links] 
+						SET 
+							title	= '$link_name', 
+							url		= '$link_url' 
+						WHERE 
+							id = '$_GET[id]'";
+			
+			$db->query($query);
+		
+			$ft->assign(array(	'CONFIRM'	=>"Link zosta³ zmodyfikowany."));
+		} else {
+			
+			$monit .= "<br /><a href=\"javascript:history.back(-1);\">powrót</a>";
+			$ft->assign('CONFIRM', $monit);
+		}
 
 		$ft->parse('ROWS',	".result_note");
 		break;
@@ -63,19 +87,18 @@ switch ($action)
 					WHERE 
 						id = '$_GET[id]'");
 		
-		$ft->assign(array(	'CONFIRM'	=>"Link zosta³ usuniêty."));
-
+		$ft->assign('CONFIRM', "Link zosta³ usuniêty.");
 		$ft->parse('ROWS', ".result_note");
 		break;
 
 	default:
-		$query = "SELECT * FROM 
+		$query = "	SELECT * FROM 
 						$mysql_data[db_table_links] 
 					ORDER BY 
 						id 
 					ASC";
+		
 		$db->query($query);
-	
 	
 		// Pêtla wyswietlaj¹ca wszystkie wpisy + stronnicowanie ich
 		while($db->next_record()) {
