ac2516b8585bef1197e60b06c00a8e2345880e03
lark <lark@68905588-48f1-0310-a723-09cfc018eb8a>
2005-04-06 11:01:29 +0000

    Przy listowaniu wpisow w danej kategorii nie pokazywal sie link do komentarzy. Poprawione

--- trunk/modules/category_view.php
+++ trunk/modules/category_view.php
@@ -81,7 +81,7 @@ if(is_numeric($_GET['id'])) {
             }
                     
 
-            if($comments_allow) {
+            if(!$comments_allow) {
             
                 /*
                  *html tylko w .tpl
@@ -90,7 +90,7 @@ if(is_numeric($_GET['id'])) {
                 $ft->assign('COMMENTS_ALLOW', '<br />');
             } else {
         
-                if($comments == 0) {
+                if(!$comments == 0) {
             
                     // template prepare
                     $ft->define('comments_link_empty', 'comments_link_empty.tpl');
