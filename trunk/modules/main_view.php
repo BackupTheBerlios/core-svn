0ef8198e3e38a84700ddcd646aa3230496da270b
lark <lark@68905588-48f1-0310-a723-09cfc018eb8a>
2005-04-06 12:07:16 +0000

    Ujednolicenie sprintf() poprawki w kodzie

--- trunk/modules/main_view.php
+++ trunk/modules/main_view.php
@@ -32,107 +32,112 @@ $db->query($query);
 if($db->num_rows() !== 0) {
 
 	while($db->next_record()) {
-	
-		$date 			= $db->f("date");
-		$title 			= $db->f("title");
-		$text 			= $db->f("text");
-		$author 		= $db->f("author");
-		$id 			= $db->f("id");
-		$c_id			= $db->f("c_id");
-		$image			= $db->f("image");
-		$comments_allow = $db->f("comments_allow");
-	
-		$c_name 		= $db->f("category_name");
-		$c_id 			= $db->f("category_id");
-	
-		$comments 		= $db->f("comments");
-	
-		// konwersja daty na bardziej ludzki format
-		$date		= coreDateConvert($date);
-	
-		if($c_name === "art&design") { 
-		
-			$c_name = "art&amp;design";
-		}
-	
-		$ft->assign(array(	'DATE'				=>$date,
-							'NEWS_TITLE'		=>$title,
-							'NEWS_TEXT'			=>$text,
-							'NEWS_AUTHOR'		=>$author,
-							'NEWS_ID'			=>$id,
-							'CATEGORY_NAME'		=>$c_name,
-							'NEWS_CATEGORY'		=>$c_id));
-
-		if($pagination['page_string'] !== "") {
-		
-			$ft->assign('STRING', "<b>Id¼ do strony:</b> " . $pagination['page_string']);
-		} else {
-		
-			$ft->assign('STRING', $pagination['page_string']);
-		}
-	
-		if(($comments_allow) == 0 ) {
-			
-			$ft->assign('COMMENTS_ALLOW', '<br />');
-		} else {
-		
-			if($comments == 0) {
-				
-				// template prepare
-				$ft->define('comments_link_empty', "comments_link_empty.tpl");
-				
-				$ft->parse('COMMENTS_ALLOW', "comments_link_empty");
-			} else {
-				
-				// template prepare
-				$ft->define('comments_link_alter', "comments_link_alter.tpl");
-				$ft->assign('COMMENTS', $comments);
-				
-				$ft->parse('COMMENTS_ALLOW', "comments_link_alter");
-			}	
-		}
-	
-		if(empty($image)) {
-
-			$ft->assign('IMAGE', '');
-
-		} else {
-			
-			if(is_file('photos/' . $image)) {
-				list($width, $height) = getimagesize("photos/" . $image);
-			
-				// wysoko¶æ, szeroko¶æ obrazka
-				$ft->assign(array(	'WIDTH'		=>$width,
-									'HEIGHT'	=>$height));
-		
-				if($width > 440) {
-				
-					// template prepare
-					$ft->define('image_alter', "image_alter.tpl");
-					$ft->assign('UID', $id);
-				
-					$ft->parse('IMAGE', "image_alter");
-				} else {
-			
-					// template prepare
-					$ft->define('image_main', "image_main.tpl");
-					$ft->assign('IMAGE_NAME', $image);
-
-					$ft->parse('IMAGE', "image_main");
-				}
-			}
-		}		
-				
-		$ft->parse('ROWS', ".rows");
-	}	
+	    
+	    $date              = $db->f("date");
+	    $title             = $db->f("title");
+	    $text              = $db->f("text");
+	    $author            = $db->f("author");
+	    $id                = $db->f("id");
+	    $c_id              = $db->f("c_id");
+	    $image             = $db->f("image");
+	    $comments_allow    = $db->f("comments_allow");
+	    
+	    $c_id              = $db->f("category_id");
+	    
+	    $comments          = $db->f("comments");
+	    
+	    // konwersja daty na bardziej ludzki format
+	    $date      = coreDateConvert($date);
+	    
+	    $c_name    = str_replace('&', '&amp;', $db->f('category_name'));
+	    
+	    $ft->assign(array(
+	       'DATE'          =>$date,
+	       'NEWS_TITLE'    =>$title,
+	       'NEWS_TEXT'     =>$text,
+	       'NEWS_AUTHOR'   =>$author,
+	       'NEWS_ID'       =>$id,
+	       'CATEGORY_NAME' =>$c_name,
+	       'NEWS_CATEGORY' =>$c_id
+	    ));
+	    
+	    if($pagination['page_string'] !== "") {
+	        
+	        $ft->assign('STRING', "<b>Id¼ do strony:</b> " . $pagination['page_string']);
+	    } else {
+	        
+	        $ft->assign('STRING', $pagination['page_string']);
+	    }
+	    
+	    if(($comments_allow) == 0 ) {
+	        
+	        $ft->assign('COMMENTS_ALLOW', '<br />');
+	    } else {
+	        
+	        if($comments == 0) {
+	            
+	            // template prepare
+	            $ft->define('comments_link_empty', "comments_link_empty.tpl");
+	            
+	            $ft->parse('COMMENTS_ALLOW', "comments_link_empty");
+	        } else {
+	            
+	            // template prepare
+	            $ft->define('comments_link_alter', "comments_link_alter.tpl");
+	            $ft->assign('COMMENTS', $comments);
+	            
+	            $ft->parse('COMMENTS_ALLOW', "comments_link_alter");
+	        }
+	    }
+	    
+	    if(empty($image)) {
+	        
+	        $ft->assign('IMAGE', '');
+	    
+	    } else {
+	        
+	        $img_path = get_root() . '/photos/' . $image;
+	        
+	        if(is_file($img_path)) {
+	            
+	            list($width, $height) = getimagesize($img_path);
+	            
+	            // wysoko¶æ, szeroko¶æ obrazka
+	            $ft->assign(array(
+                    'WIDTH'     =>$width,
+                    'HEIGHT'    =>$height
+	            ));
+	            
+	            if($width > 440) {
+	                
+	                // template prepare
+	                $ft->define('image_alter', "image_alter.tpl");
+	                $ft->assign('UID', $id);
+	                
+	                $ft->parse('IMAGE', "image_alter");
+	            
+	            } else {
+	                
+	                // template prepare
+	                $ft->define('image_main', "image_main.tpl");
+	                $ft->assign('IMAGE_NAME', $image);
+	                
+	                $ft->parse('IMAGE', "image_main");
+	            }
+	        }
+	    }
+	    
+	    $ft->parse('ROWS', ".rows");
+	}
 } else {
-	
-	// Obs³uga b³êdu, kiedy w bazie danych nie ma jeszcze ¿adnego wpisu
-	$ft->assign(array(	'QUERY_FAILED'	=>"W bazie danych nie ma ¿adnego wpisu. Mo¿esz siê <a href=\"administration\">zalogowaæ</a>.",
-						'STRING'		=>""));
-						
-	$ft->parse('ROWS', ".query_failed");
+    
+    // Obs³uga b³êdu, kiedy w bazie danych nie ma jeszcze ¿adnego wpisu
+    $ft->assign(array(
+        'QUERY_FAILED'  =>"W bazie danych nie ma ¿adnego wpisu. Mo¿esz siê <a href=\"administration\">zalogowaæ</a>.",
+        'STRING'        =>""
+    ));
+    
+    $ft->parse('ROWS', ".query_failed");
 }
-
 
 ?>
--- trunk/modules/photo_view.php
+++ trunk/modules/photo_view.php
@@ -1,23 +1,27 @@
 <?php
 
 $query = sprintf("
-			SELECT * FROM 
-				$mysql_data[db_table] 
-			WHERE 
-				id = '%1\$d' 
-			LIMIT 1", $_GET['id']);
+    SELECT * FROM 
+        %1\$s 
+    WHERE 
+        id = '%2\$d' 
+    LIMIT 1", 
+
+    $mysql_data['db_table'],
+    $_GET['id']
+);
 
 $db->query($query);
 $db->next_record();
 
-$image	= $db->f("image");
+$image  = $db->f("image");
 
 list($width, $height) = getimagesize("photos/" . $image);
 
 $ft->assign(array(
-				'IMAGE_NAME'	=>$image,
-				'IMAGE_WIDTH'	=>$width,
-				'IMAGE_HEIGHT'	=>$height
+    'IMAGE_NAME'    =>$image,
+    'IMAGE_WIDTH'   =>$width,
+    'IMAGE_HEIGHT'  =>$height
 ));
 
 $ft->define('photo_view', "photo_view.tpl");
