7035ea0209f4389e616de3e255b954b4486f7bcc
lark <lark@68905588-48f1-0310-a723-09cfc018eb8a>
2005-04-07 12:12:16 +0000

    Ujednolicenie zapytan, dodanie mozliwosci edycji zmiennej $max_photo_width

--- trunk/administration/modules/core_configuration.php
+++ trunk/administration/modules/core_configuration.php
@@ -35,34 +35,86 @@ switch ($action) {
 		}
 		
 		// egzemplarz klasy validuj±cej ustawienia core
+		/*
 		$conf = new configValidate();
 		
-		//$conf->checkNumeric($_POST['mainposts_per_page']);
-		//$conf->checkValue($_POST['mainposts_per_page'], 3, 10, "na stronie g³ównej");
-		//$conf->checkValue($_POST['editposts_per_page'], 15, 25, "w czê¶ci administracyjnej");
+		$conf->checkNumeric($_POST['mainposts_per_page']);
+		$conf->checkValue($_POST['mainposts_per_page'], 3, 10, "na stronie g³ównej");
+		$conf->checkValue($_POST['editposts_per_page'], 15, 25, "w czê¶ci administracyjnej");
+		*/
 		
 		if(empty($conf->monit)) {
-			
-			// set {MAINPOSTS_PER_PAGE} variable
-			// liczba listowanych wpisów w na stronie g³ównej::db
-			$db->query("	UPDATE $mysql_data[db_table_config] 
-							SET config_value = '$_POST[mainposts_per_page]' 
-							WHERE config_name = 'mainposts_per_page'");
+		    
+            // set {MAINPOSTS_PER_PAGE} variable
+            // liczba listowanych wpisów w na stronie g³ównej::db
+            $query = sprintf("
+                UPDATE 
+                    %1\$s 
+				SET 
+				    config_value = '%2\$d' 
+				WHERE 
+				    config_name = '%3\$s'",
+            
+                $mysql_data['db_table_config'], 
+                $_POST['mainposts_per_page'], 
+                'mainposts_per_page'
+            );
+            
+            $db->query($query);
 			
 			// set {TITLE_PAGE} variable
 			// liczba listowanych wpisów w na stronie g³ównej::db
-			$db->query("	UPDATE $mysql_data[db_table_config] 
-							SET config_value = '$_POST[title_page]' 
-							WHERE config_name = 'title_page'");
+			$query = sprintf("
+                UPDATE 
+                    %1\$s 
+				SET 
+				    config_value = '%2\$s' 
+				WHERE 
+				    config_name = '%3\$s'",
+            
+                $mysql_data['db_table_config'], 
+                $_POST['title_page'], 
+                'title_page'
+            );
+            
+            $db->query($query);
 		
 			// set {EDITOSTS_PER_PAGE} variable
 			// liczba listowanych wpisów w na stronie g³ównej::db
-			$db->query("	UPDATE $mysql_data[db_table_config] 
-							SET config_value = '$_POST[editposts_per_page]' 
-							WHERE config_name = 'editposts_per_page'");
-		
+			$query = sprintf("
+                UPDATE 
+                    %1\$s 
+				SET 
+				    config_value = '%2\$d' 
+				WHERE 
+				    config_name = '%3\$s'",
+            
+                $mysql_data['db_table_config'], 
+                $_POST['editposts_per_page'], 
+                'editposts_per_page'
+            );
+            
+            $db->query($query);
+            
+            // set {MAX_PHOTO_WIDTH} variable
+            // maksymalna szerko¶æ zdjêcia do³±czonego do wpisu, 
+            // jakie jest wy¶wietlane na stronie g³ównej::db
+			$query = sprintf("
+                UPDATE 
+                    %1\$s 
+				SET 
+				    config_value = '%2\$d' 
+				WHERE 
+				    config_name = '%3\$s'",
+            
+                $mysql_data['db_table_config'], 
+                $_POST['max_photo_width'], 
+                'max_photo_width'
+            );
+            
+            $db->query($query);
+			
 			$ft->assign('CONFIRM', "Dane zosta³y zmodyfikowane.");
-		
 			$ft->parse('ROWS', ".result_note");
 		
 		} else {
@@ -75,43 +127,95 @@ switch ($action) {
 		}
 		break;
 
-	default:
-		// set {MAINPOSTS_PER_PAGE} variable
-		// liczba listowanych wpisów w na stronie g³ównej::db
-		$db->query("	SELECT config_value 
-						FROM $mysql_data[db_table_config]
-						WHERE config_name = 'mainposts_per_page'");
-		
-		$db->next_record();
+    default:
+    
+        // set {MAINPOSTS_PER_PAGE} variable
+        // liczba listowanych wpisów w na stronie g³ównej::db
+        $query = sprintf("
+            SELECT 
+                config_value 
+            FROM 
+                %1\$s 
+            WHERE 
+                config_name = '%2\$s'",
+        
+            $mysql_data['db_table_config'], 
+            'mainposts_per_page'
+        );
+            
+        $db->query($query);
+        $db->next_record();
+        
 		$mainposts_per_page = $db->f("config_value");
-			
-		$ft->assign('MAINPOSTS_PER_PAGE', $mainposts_per_page);
 		
 		// set {EDITPOSTS_PER_PAGE} variable
 		// liczba listowanych wpisów w czê¶ci administracyjnej::db
-		$db->query("	SELECT config_value 
-						FROM $mysql_data[db_table_config] 
-						WHERE config_name = 'editposts_per_page'");
+        $query = sprintf("
+            SELECT 
+                config_value 
+            FROM 
+                %1\$s 
+            WHERE 
+                config_name = '%2\$s'",
+        
+            $mysql_data['db_table_config'], 
+            'editposts_per_page'
+        );
 		
-		$db->next_record();
+		$db->query($query);
+        $db->next_record();
+        
 		$editposts_per_page = $db->f("config_value");
-		
-		$ft->assign('EDITPOSTS_PER_PAGE', $editposts_per_page);
 		
 		// set {TITLE_PAGE} variable
 		// tytu³ strony, wy¶wietlany w miejscu title::db
-		$db->query("	SELECT config_value 
-						FROM $mysql_data[db_table_config] 
-						WHERE config_name = 'title_page'");
+        $query = sprintf("
+            SELECT 
+                config_value 
+            FROM 
+                %1\$s 
+            WHERE 
+                config_name = '%2\$s'",
+        
+            $mysql_data['db_table_config'], 
+            'title_page'
+        );
 		
-		$db->next_record();
+		$db->query($query);
+        $db->next_record();
+        
 		$title_page = $db->f("config_value");
 		
-		$ft->assign('TITLE_PAGE', $title_page);
+		// set {MAX_PHOTO_WIDTH} variable
+		// maksymalna szerko¶æ zdjêcia do³±czonego do wpisu, 
+		// jakie jest wy¶wietlane na stronie g³ównej::db
+        $query = sprintf("
+            SELECT 
+                config_value 
+            FROM 
+                %1\$s 
+            WHERE 
+                config_name = '%2\$s'",
+        
+            $mysql_data['db_table_config'], 
+            'max_photo_width'
+        );
+		
+		$db->query($query);
+        $db->next_record();
+        
+		$max_photo_width = $db->f("config_value");
+		
+		// Ustawiamy zmienne
+        $ft->assign(array(
+            'MAINPOSTS_PER_PAGE'    =>$mainposts_per_page,
+            'EDITPOSTS_PER_PAGE'    =>$editposts_per_page,
+            'TITLE_PAGE'            =>$title_page,
+            'MAX_PHOTO_WIDTH'       =>$max_photo_width
+        ));
 			
 		// w przypadku braku akcji wy¶wietlanie formularza
 		$ft->define('form_configuration', "form_configuration.tpl");
 		$ft->parse('ROWS', ".form_configuration");
 }
-
 ?>
