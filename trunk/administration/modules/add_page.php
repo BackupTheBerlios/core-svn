8e5352dd438a4b7e1b91f12de5912a0e835b03cd
lark <lark@68905588-48f1-0310-a723-09cfc018eb8a>
2005-04-10 18:42:58 +0000

    Usuniecie zbednej instancji klasy, ujednolicenie zapytan poprzez sprintf(), zmiana sposobu parsowania szablonow na define_dynamic()

--- trunk/administration/modules/add_page.php
+++ trunk/administration/modules/add_page.php
@@ -3,8 +3,6 @@
 // deklaracja zmiennej $action::form
 $action = empty($_GET['action']) ? '' : $_GET['action'];
 
-$db = new DB_SQL;
-
 switch($action) {
 	
 	case "add":
@@ -14,17 +12,29 @@ switch($action) {
 		$published 	= $_POST['published'];
 		$page_id	= $_POST['category_id'];
 		
-		$query = "	INSERT INTO 
-						$mysql_data[db_table_pages] 
-					VALUES 
-						('', '$page_id', '$title', '$text', '', '$published')";
+		$query = sprintf("
+            INSERT INTO 
+                %1\$s 
+            VALUES 
+                ('', '%2\$d', '%3\$s', '%4\$s', '', '%5\$s')", 
+		
+            $mysql_data['db_table_pages'], 
+            $page_id, 
+            $title, 
+            $text, 
+            $published
+        );
 		
 		$db->query($query);
 		
-		$query = "	SELECT MAX(id) 
-						as maxid 
-					FROM 
-						$mysql_data[db_table_pages]";
+		$query = sprintf("
+            SELECT MAX(id) 
+                as maxid 
+            FROM 
+                %1\$s", 
+
+            $mysql_data['db_table_pages']
+        );
 		
  		$db->query($query);
  		$db->next_record();
@@ -44,12 +54,18 @@ switch($action) {
 				echo $up->error;
 			} else {
 			
-				$query = "	UPDATE 
-								$mysql_data[db_table_pages] 
-							SET 
-								image = '$file' 
-							WHERE 
-								id = '$id'";
+				$query = sprintf("
+                    UPDATE 
+                        %1\$s 
+                    SET 
+                        image = '%2\$s' 
+                    WHERE 
+                        id = '%3\$d'", 
+			    
+                    $mysql_data['db_table_pages'],
+                    $file,
+                    $id
+                );
 				
 				$db->query($query);
 				
@@ -62,37 +78,46 @@ switch($action) {
 		$ft->parse('ROWS',	".result_note");
 		break;
 
-	default:	
-	$query	= "	SELECT 
-					id, parent_id, title 
-				FROM 
-					$mysql_data[db_table_pages] 
-				WHERE 
-					published = 'Y' 
-				AND 
-					parent_id = '0'
-				ORDER BY 
-					id 
-				ASC";
+	default:
+		
+	$query = sprintf("
+        SELECT 
+            id, parent_id, title 
+        FROM 
+            %1\$s 
+        WHERE 
+            published = 'Y' 
+        AND 
+            parent_id = '%2\$d' 
+        ORDER BY 
+            id 
+        ASC", 
+	
+	   $mysql_data['db_table_pages'],
+	   0);
 	
 	$db->query($query);
 	while($db->next_record()) {
 		
-		$page_id 		= $db->f("id");
-		$parent_id 		= $db->f("parent_id");
-		$title 			= $db->f("title");
+		$page_id      = $db->f("id");
+		$parent_id    = $db->f("parent_id");
+		$title        = $db->f("title");
 	
-		$ft->assign(array(	'C_ID'		=>$page_id,
-							'C_NAME'	=>$title));
+		$ft->assign(array(
+            'C_ID'		=>$page_id,
+            'C_NAME'	=>$title
+        ));
 							
-		$ft->define('page_categoryoption', "page_categoryoption.tpl");				
-		$ft->parse('CATEGORY_ROWS', ".page_categoryoption");
+		$ft->define("form_pageadd", "form_pageadd.tpl");
+        $ft->define_dynamic("page_row", "form_pageadd");
+        
+        $ft->parse('ROWS',	".page_row");
+        
 		get_addpage_cat($page_id, 2);				
 	
 	}
 
-	$ft->define('form_pageadd', "form_pageadd.tpl");
-	$ft->parse('ROWS',	".form_pageadd");
+	$ft->parse('ROWS',	"form_pageadd");
 }
 
 ?>
