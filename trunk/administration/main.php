b8354ac0b8ae0d1ada7c28019415897ac47810b0
lark <lark@68905588-48f1-0310-a723-09cfc018eb8a>
2005-04-11 21:42:50 +0000

    Podmiana plikow

---
+++ trunk/administration/main.php
@@ -0,0 +1,165 @@
+<?php
+
+session_register("login");
+session_register("loggedIn");
+
+if(!isset($_SESSION["loggedIn"])){
+	
+	header("Location: index.php");
+	exit;
+}
+
+// warto¶æ pocz±tkowa zmiennej $start -> potrzebna przy stronnicowaniu
+$start = isset($_GET['start']) ? intval($_GET['start']) : 0;
+
+define('PATH_TO_CLASSES', 'classes');
+
+require(PATH_TO_CLASSES. '/cls_db_mysql.php');
+require(PATH_TO_CLASSES. '/cls_upload.php');
+
+require("inc/config.php");
+include("../inc/main_pagination.php");
+include("../inc/main_functions.php");
+
+require(PATH_TO_CLASSES. '/cls_fast_template.php');
+require(PATH_TO_CLASSES. '/cls_permissions.php');
+
+// egzemplarz klasy obs³uguj±cej bazê danych Core
+$db = new DB_SQL;
+
+// pobieramy poziom uprawnieñ
+$query = sprintf("
+    SELECT 
+        permission_level 
+    FROM 
+        %1\$s 
+    WHERE 
+        login = '%2\$s'",
+    
+    $mysql_data['db_table_users'], 
+    $_SESSION['login']
+);
+    
+$db->query($query);
+$db->next_record();
+
+$privileges = $db->f('permission_level');
+
+// egzemplarz klasy do obs³ugi uprawnieñ
+$perms      = new permissions();
+$permarr    = $perms->getPermissions($privileges);
+
+// inicjowanie klasy, wkazanie katalogu przechowuj±cego szablony
+$ft = new FastTemplate("./templates/tpl");
+
+// tablica definicji u¿ytych plików *.tpl
+$ft->define(array(
+    'index'         =>"index.tpl",
+    'main_loader'   =>"main_loader.tpl",
+    'main_site'     =>"main_site.tpl",
+    
+    // szablon obs³uguj±cy error handlera, jak i dodatkowe komunikaty
+    'result_note'   =>"result_note.tpl"
+));
+		
+$ft->assign(array(
+    'PAGE_TITLE'    =>"CORE - panel administracyjny",
+    'LOGGED_IN'     =>$_SESSION['login']
+));
+
+$inc_modules = array(
+    "header_menu",
+    "subcat_menu"
+);
+
+foreach($inc_modules as $module) {
+    
+    // ³adowanie dodatkowych modu³ów
+    include('modules/' . $module . '.php');
+}
+
+// prze³±cznica ³adowanej tre¶ci					
+$p = empty($_GET['p']) ? '' : $_GET['p'];
+switch($p){
+	
+	// dodawanie kolejnego wpisu
+	case '1': 
+		include('modules/add_note.php');
+		break;
+
+	// edycja/usuwanie istniej±cych wpisów
+	case '2': 
+		include('modules/edit_note.php');
+		break;
+		
+	// dodawanie kolejnej strony
+	case '3': 
+		include('modules/add_page.php');
+		break;
+
+	// edycja/usuwanie istniej±cych wpisów
+	case '4': 
+		include('modules/edit_page.php');
+		break;
+		
+	// edycja/usuwanie istniej±cych komentarzy
+	case '5': 
+		include('modules/edit_comments.php');
+		break;
+		
+	// statystycznie najczê¶ciej komentowane wpisy
+	case '6': 
+		include('modules/most_comments.php');
+		break;	
+		
+	// dodanie nowego u¿ytkownika systemu	
+	case '7': 
+		include('modules/add_user.php');
+		break;
+		
+	// dodanie nowej kategorii	
+	case '8': 
+		include('modules/add_category.php');
+		break;
+		
+	// edycja|usuwanie istniej±cych kategorii	
+	case '9': 
+		include('modules/edit_category.php');
+		break;
+		
+	// konfiguracja core	
+	case '10': 
+		include('modules/core_configuration.php');
+		break;
+		
+	// dodanie nowego linku	
+	case '11': 
+		include('modules/add_links.php');
+		break;
+		
+	// edycja|usuwanie istniej±cych linków	
+	case '12': 
+		include('modules/edit_links.php');
+		break;
+		
+	// edycja|usuwanie u¿ytkowników	
+	case '13': 
+		include('modules/edit_users.php');
+		break;
+		
+	// edycja|usuwanie szablonów	
+	case '14': 
+		include('modules/edit_templates.php');
+		break;
+	
+	// domy¶lnie	
+	default:
+		include('modules/main.php');
+		break;
+}
+
+$ft->parse('MAIN_CONTENT', array("main_loader", "index"));
+
+$ft->FastPrint();
+exit;
+?>
