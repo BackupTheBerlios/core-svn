d535d33cb75f1d68a3343fe5b5762d494cc9013c
lark <lark@68905588-48f1-0310-a723-09cfc018eb8a>
2005-04-11 11:18:14 +0000

    Zmiana sposobu parsowania szablonow z lista tpl'i do edycji, na define_dynamic()

--- trunk/administration/modules/edit_templates.php
+++ trunk/administration/modules/edit_templates.php
@@ -79,44 +79,46 @@ switch($action) {
 
 //lista szablonów
 if(!isset($_GET['path'])) {
-			
-	$path = "../templates/main/tpl/";
+    
+    $path = "../templates/main/tpl/";
 } else {
-			
-	$path = "../templates/" . $_GET['path'] . "/tpl/";
+    
+    $path = "../templates/" . $_GET['path'] . "/tpl/";
 }
 
 $dir = @dir($path);
+
+$ft->define("editlist_templates", "editlist_templates.tpl");
+$ft->define_dynamic("template_row", "editlist_templates");
+
 while($file = $dir->read()) {
-  $ext = str_getext($file, false);
-  
-  if(
-      !in_array($ext, array('php', 'txt', 'html'))
-      //&& !in_array($file, array('.', '..', '.svn'))
-      && is_file($path . $file)
-  ) {
-		$file = explode('.', $file);
-				
-		$ft->assign(array(	'FILE'		=>$file[0] . "." . $file[1],
-							'FILE_PATH'	=>$file[0]));
-									
     
-    //jesli plik nie jest zapisywalny, to tpl z gwiazdka
-    if (is_writeable($path . $file)) {
-
-      $ft->define('filelist', "filelist.tpl");
-    } else {
-
-      $ft->define('filelist', "filelist_closed.tpl");
+    $ext = str_getext($file, false);
+    
+    if(!in_array($ext, array('php', 'txt', 'html')) && is_file($path . $file)) {
+        
+        $file = explode('.', $file);
+        $ft->assign(array(
+            'FILE'		=>$file[0] . "." . $file[1],
+            'FILE_PATH'	=>$file[0]
+        ));
+        
+        //jesli plik nie jest zapisywalny, to tpl z gwiazdka
+        if(is_writeable($path . $file)) {
+            
+            $ft->assign('STAR', '');
+            $ft->parse('SUB_CONTENT', ".template_row");
+            
+        } else {
+            
+            $ft->assign('STAR', '*');
+            $ft->parse('SUB_CONTENT', ".template_row");
+        }
     }
-      
-    
-		$ft->parse('FILEBOX_CONTENT', ".filelist");
-	}
 }
-		
-$ft->define('filebox', "filebox.tpl");
-$ft->parse('SUB_CONTENT', ".filebox");
+
+$ft->parse('SUB_CONTENT', "editlist_templates");
 		
 $dir->close();
+
 ?>
