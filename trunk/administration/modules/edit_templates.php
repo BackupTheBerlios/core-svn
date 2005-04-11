9d8ed2011a218f3325ffaac37b23eecdd222b714
lark <lark@68905588-48f1-0310-a723-09cfc018eb8a>
2005-04-11 11:50:37 +0000

    Zmiana sposobu parsowania szablonow. define_dynamic()

--- trunk/administration/modules/edit_templates.php
+++ trunk/administration/modules/edit_templates.php
@@ -3,75 +3,77 @@
 // deklaracja zmiennej $action::form
 $action = empty($_GET['action']) ? '' : $_GET['action'];
 
-// inicjalizacja instancji klasy DB_SQL
-$db = new DB_SQL;
+$ft->define("editlist_templates", "editlist_templates.tpl");
+$ft->define_dynamic("template_row", "editlist_templates");
 
 switch($action) {
-	
-	case "add":
-	
-		$template   = $_POST['template_name'];
-		$text		= $_POST['text'];
-		
-		$tpl = 	'../templates/main/tpl/' . $template . '.tpl';
-		
-		if(is_writeable($tpl)) {
-			
-			$fp	= fopen($tpl, 'w+');
-			fwrite($fp, stripslashes($text));
-			fclose($fp);
-		
-			$ft->assign('CONFIRM', "Szablon zosta³ Zapisany.");
-		} else {
-			
-			$ft->assign('CONFIRM', "Nie uda³o siê edytowaæ szablonu.");
-		}
-		
-		$ft->parse('ROWS',	".result_note");
-		break;
+    
+    case "add":
+    
+        $template   = $_POST['template_name'];
+        $text		= $_POST['text'];
+        
+        $tpl = 	'../templates/main/tpl/' . $template . '.tpl';
+        
+        if(is_writeable($tpl)) {
+            
+            $fp	= fopen($tpl, 'w+');
+            fwrite($fp, stripslashes($text));
+            fclose($fp);
+            
+            $ft->assign('CONFIRM', "Szablon zosta³ Zapisany.");
+        } else {
+            
+            $ft->assign('CONFIRM', "Nie uda³o siê edytowaæ szablonu.");
+        }
+        
+        $ft->parse('ROWS',	".result_note");
+        break;
 		
 	case "show":
 
-		$ft->define('form_templateedit', "form_templateedit.tpl");
-			
-		$tpl 		= empty($_GET['id']) ? '' : $_GET['id'];
-		$template 	= get_root() . "/templates/main/tpl/" . $tpl . ".tpl";
-    if (!is_writeable($template)) {
-      
-      $ft->assign('WRITE_ERROR', 'Brak mo¿liwo¶ci zapisu zmian w tym szablonie!');
-    } else {
-
-      $ft->assign('WRITE_ERROR', '');
-    }
-		
-		$file_content = @file_get_contents($template);
-    if ($file_content) {
-		
-      // Sztywna obs³uga </textarea> w szablonie, aby by³
-      // on wy¶wietlany poprawnie w polu formularza
-      $file_content = str_replace('</textarea>', '&lt;/textarea>', $file_content);
-      
-      // Zabronimy FT ukrywanie nie przydzielonych zmiennych
-      // dziêki temu widaæ je przy edycji danego szablonu
-      $ft->strict();
-      
-      $ft->assign(array(	'FILE_CONTENT'	=>$file_content,
+        $ft->define('form_templateedit', "form_templateedit.tpl");
+        
+        $tpl 		= empty($_GET['id']) ? '' : $_GET['id'];
+        $template 	= get_root() . "/templates/main/tpl/" . $tpl . ".tpl";
+        
+        if(!is_writeable($template)) {
+            
+            $ft->assign('WRITE_ERROR', 'Brak mo¿liwo¶ci zapisu zmian w tym szablonie!');
+        
+        } else {
+            
+            $ft->assign('WRITE_ERROR', '');
+        }
+        
+        $file_content = @file_get_contents($template);
+        
+        if ($file_content) {
+            
+            // Sztywna obs³uga </textarea> w szablonie, aby by³
+            // on wy¶wietlany poprawnie w polu formularza
+            $file_content = str_replace('</textarea>', '&lt;/textarea>', $file_content);
+            
+            // Zabronimy FT ukrywanie nie przydzielonych zmiennych
+            // dziêki temu widaæ je przy edycji danego szablonu
+            $ft->strict();
+            
+            $ft->assign(array(
+                'FILE_CONTENT'	=>$file_content,
                 'TEMPLATE'		=>"/ " . $tpl . ".tpl",
-                'TEMPLATE_NAME'	=>$tpl));
-    } else {
-
-      $ft->assign(array(	'FILE_CONTENT'	=> '',
+                'TEMPLATE_NAME'	=>$tpl
+            ));
+        } else {
+            
+            $ft->assign(array(
+                'FILE_CONTENT'	=> '',
                 'TEMPLATE'		=> '',
-                'TEMPLATE_NAME'	=> ''));
-    }
-							
-		$ft->parse('ROWS',	".form_templateedit");
-		break;
+                'TEMPLATE_NAME'	=> ''
+            ));
+        }
+        break;
 
 	default:
-
-		$ft->define('form_templateedit', "form_templateedit.tpl");
-		$ft->parse('ROWS',	".form_templateedit");
 
 }
 
@@ -82,14 +84,10 @@ if(!isset($_GET['path'])) {
     
     $path = "../templates/main/tpl/";
 } else {
-    
     $path = "../templates/" . $_GET['path'] . "/tpl/";
 }
 
 $dir = @dir($path);
-
-$ft->define("editlist_templates", "editlist_templates.tpl");
-$ft->define_dynamic("template_row", "editlist_templates");
 
 while($file = $dir->read()) {
     
@@ -107,17 +105,17 @@ while($file = $dir->read()) {
         if(is_writeable($path . $file)) {
             
             $ft->assign('STAR', '');
-            $ft->parse('SUB_CONTENT', ".template_row");
+            $ft->parse('ROWS', ".template_row");
             
         } else {
             
             $ft->assign('STAR', '*');
-            $ft->parse('SUB_CONTENT', ".template_row");
+            $ft->parse('ROWS', ".template_row");
         }
     }
 }
 
-$ft->parse('SUB_CONTENT', "editlist_templates");
+$ft->parse('ROWS', "editlist_templates");
 		
 $dir->close();
 
