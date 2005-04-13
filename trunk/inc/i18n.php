605b978af040b7a2ba00fd22961324dd575b2eb3
mysz <mysz@68905588-48f1-0310-a723-09cfc018eb8a>
2005-04-13 23:07:33 +0000

    dodany plik - tutaj trzymane beda wszystkie komunikaty jakie beda wysylane do usera

---
+++ trunk/inc/i18n.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ * all messages
+ *
+ * format:
+ * $i18n['filename_without_extension'][(int)]
+ */
+
+
+$i18n = array();
+
+$i18n['index'] = array();
+$i18n['index'][0] = 'Brak wybranego skina do systemu <strong>CORE</strong>.';
+$i18n['index'][] = 'Based on <a href="http://core.no1-else.com/" target="_blank">Core</a>';
+
+$i18n['design'] = array();
+$i18n['design'][0] = 'Brak wybranego skina do systemu <strong>CORE</strong>.';
+
+$i18n['rss'] = array();
+$i18n['rss'][0] = './dev-log - RSS feed';
+$i18n['rss'][1] = './dev-log | about work';
+$i18n['rss'][2] = 'pl';
+$i18n['rss'][3] = 'Copyright (c) 2004 core | £ukasz Skowroñ';
+
+$i18n['rsscomments'] = array();
+$i18n['rsscomments'][0] = './dev-log - comments RSS feed';
+$i18n['rsscomments'][1] = './dev-log | about work';
+$i18n['rsscomments'][2] = 'pl';
+$i18n['rsscomments'][3] = 'Copyright (c) 2004 core | £ukasz Skowroñ';
+
+?>
