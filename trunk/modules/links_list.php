dbf9d62878b0282dc5742a21ac2bd3ce1a787e15
lark <lark@68905588-48f1-0310-a723-09cfc018eb8a>
2005-04-14 12:14:02 +0000

    Blad. Gdy nie bylo linkow w bazie danych, czyli np. zaraz po instalacji Core sie calkowicie wywalalo. Blad poprawiony.

--- trunk/modules/links_list.php
+++ trunk/modules/links_list.php
@@ -13,22 +13,25 @@ $query = sprintf("
 );
 
 $db->query($query);
+if($db->num_rows() > 0) {
+    
+    while($db->next_record()) {
+    
+        $link_name  = $db->f("title");
+        $link_url   = str_replace('&', '&amp;', $db->f("url"));
+    
+        $ft->assign(array(
+            'LINK_NAME' =>$link_name,
+            'LINK_URL'  =>$link_url
+        ));
+    
+        $ft->define("links_list", "links_list.tpl");
+        $ft->define_dynamic("links_row", "links_list");
+    
+        $ft->parse('LINKS_LIST', ".links_row");
+    }
 
-while($db->next_record()) {
+    $ft->parse('LINKS_LIST', 'links_list');
     
-    $link_name  = $db->f("title");
-    $link_url   = str_replace('&', '&amp;', $db->f("url"));
-    
-    $ft->assign(array(
-        'LINK_NAME' =>$link_name,
-        'LINK_URL'  =>$link_url
-    ));
-    
-    $ft->define("links_list", "links_list.tpl");
-    $ft->define_dynamic("links_row", "links_list");
-    
-    $ft->parse('LINKS_LIST', ".links_row");
 }
-
-$ft->parse('LINKS_LIST', 'links_list');
 ?>
