ac886918938d430576732ed42f4f20968f9b1f72
lark <lark@68905588-48f1-0310-a723-09cfc018eb8a>
2005-04-14 12:34:53 +0000

    Dodanie nowego pliku

---
+++ trunk/inc/i18n_administration.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * all messages
+ *
+ * format:
+ * $i18n['filename_without_extension'][(int)]
+ */
+
+
+$i18n = array();
+
+$i18n['index'] = array();
+$i18n['index'][0] = 'CORE - panel administracyjny';
+$i18n['index'][1] = 'Nie uzupe³niono wszystkich pól.';
+$i18n['index'][2] = 'Konto nie zosta³o jeszcze aktywowane.';
+$i18n['index'][3] = 'B³êdna nazwa u¿ytkownika, lub b³êdne has³o.';
+
+$i18n['main'] = array();
+$i18n['main'][0] = 'CORE - panel administracyjny';
+
+
+
+?>
