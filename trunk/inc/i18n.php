c11739c9b6cc77bedba6dedddca3fb1e974cdd52
lark <lark@68905588-48f1-0310-a723-09cfc018eb8a>
2005-04-14 00:45:48 +0000

    Dodanie definicji bledow

--- trunk/inc/i18n.php
+++ trunk/inc/i18n.php
@@ -43,8 +43,31 @@ $i18n['comments_add'][1] = 'Proszê podaæ poprawny adres e-mail.<br />';
 $i18n['comments_add'][2] = 'Komentarz zosta³ dodany.';
 $i18n['comments_add'][3] = '<br /><a href="javascript:history.back(-1);">powrót</a>';
 
+$i18n['comments_view'] = array();
+$i18n['comments_view'][0] = 'W bazie danych nie ma wpisu o ¿±danym id.';
+$i18n['comments_view'][1] = 'Szukasz czego¶?';
+
+$i18n['main_view'] = array();
+$i18n['main_view'][0] = 'W bazie danych nie ma ¿adnego wpisu. Mo¿esz siê <a href="administration/">zalogowaæ</a>.';
+
+$i18n['newsletter'] = array();
+$i18n['newsletter'][0] = 'Twój email znajduje siê ju¿ w bazie danych.';
+$i18n['newsletter'][1] = 'Twój adres zosta³ dodany do bazy danych.';
+$i18n['newsletter'][2] = 'Twój adres nie zosta³ dodany do bazy danych.';
+$i18n['newsletter'][3] = 'W bazie danych nie ma podanego przez Ciebie adresu e-mail.';
+$i18n['newsletter'][4] = 'Twój adres zosta³ skasowany z bazy danych.';
+$i18n['newsletter'][5] = 'Twój adres nie zosta³ skasowany z bazy danych.';
+
+$i18n['pages_view'] = array();
+$i18n['pages_view'][0] = 'W bazie danych nie ma wpisu o ¿±danym id';
+
+$i18n['search'] = array();
+$i18n['search'][0] = 'Niestety nie znaleziono ¿adnego rekordu pasuj±cego do <span class="search">' . $_POST['search_word'] . '</span>.';
+$i18n['search'][1] = 'Nie podano frazy do wyszukania.';
+
 /*
 $i18n[''] = array();
 $i18n[''][] = '';
 */
+
 ?>
