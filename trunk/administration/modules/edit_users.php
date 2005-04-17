6e690753f15fecdcd42d0740ac593886637a7b1b
lark <lark@68905588-48f1-0310-a723-09cfc018eb8a>
2005-04-17 18:35:30 +0000

    Przy liscie uzytkownikow dodane pole w tabeli okreslajace poziom uprawnien i odnosniki umozliwiajace ich zmiane.

--- trunk/administration/modules/edit_users.php
+++ trunk/administration/modules/edit_users.php
@@ -193,6 +193,152 @@ switch ($action) {
 
 	default:
 	
+        $plevel = empty($_GET['plevel']) ? '' : $_GET['plevel'];
+		    
+		if(!empty($plevel)) {
+		    
+		    $query = sprintf("
+                SELECT 
+                    permission_level 
+                FROM 
+                    %1\$s 
+                WHERE 
+                    id = %2\$d 
+                LIMIT
+                    1", 
+		    
+                $mysql_data['db_table_users'],
+                $_GET['id']
+            );
+            
+            $db->query($query);
+            $db->next_record();
+            
+            $permission_level = $db->f("permission_level");
+            
+            switch ($permission_level) {
+                
+                case '1': $p_level = 1;
+                break;
+                
+                case '3': $p_level = 2;
+                break;
+                
+                case '7': $p_level = 3;
+                break;
+                
+                case '15': $p_level = 4;
+                break;
+                
+                case '31': $p_level = 5;
+                break;
+            }
+            
+            $new_permissions = new permissions;
+            
+            if($plevel == "down") {
+                
+                if($p_level == 1) {
+                        
+                    $ft->assign('CONFIRM', 'Nie mo¿esz zwiêkszyæ uprawnieñ poni¿ej 1.');
+                } else {
+                        
+                    $p_level = $p_level-1;
+                        
+                    switch ($p_level) {
+                            
+                        case '3':
+                            $new_permissions->permissions["read"]                  = TRUE;
+                            $new_permissions->permissions["write"]                 = TRUE;
+                            $new_permissions->permissions["delete"]                = TRUE;
+                            break;
+                                
+                        case '2':
+                            $new_permissions->permissions["read"]                  = TRUE;
+                            $new_permissions->permissions["write"]                 = TRUE;
+                            break;
+                                
+                        case '1':
+                            $new_permissions->permissions["read"]                  = TRUE;
+                            break;
+                    }
+                        
+                    $mask   = $new_permissions->toBitmask();
+                        
+                    $query  = sprintf("
+                        UPDATE 
+                            %1\$s 
+                        SET 
+                            permission_level = %2\$d 
+                        WHERE 
+                            id = %3\$d", 
+                        
+                        $mysql_data['db_table_users'],
+                        $mask,
+                        $_GET['id']
+                    );
+                    
+                    $db->query($query);
+                        
+                    $ft->assign('CONFIRM', 'Zmniejszono poziom uprawnieñ.');
+                }
+                 
+             
+            }
+            
+            if($plevel == "up") {
+                
+                if($p_level == 4) {
+                        
+                    $ft->assign('CONFIRM', 'Nie mo¿esz zwiêkszyæ uprawnieñ powy¿ej 4.');
+                } else {
+                    
+                    $p_level = $p_level+1;
+                        
+                    switch ($p_level) {
+                            
+                        case '4':
+                            $new_permissions->permissions["read"]                  = TRUE;
+                            $new_permissions->permissions["write"]                 = TRUE;
+                            $new_permissions->permissions["delete"]                = TRUE;
+                            $new_permissions->permissions["edit_templates"]        = TRUE;
+                            break;
+                                
+                        case '3':
+                            $new_permissions->permissions["read"]                  = TRUE;
+                            $new_permissions->permissions["write"]                 = TRUE;
+                            $new_permissions->permissions["delete"]                = TRUE;
+                            break;
+                                
+                        case '2':
+                            $new_permissions->permissions["read"]                  = TRUE;
+                            $new_permissions->permissions["write"]                 = TRUE;
+                            break;
+                    }
+                        
+                    $mask   = $new_permissions->toBitmask();
+                        
+                    $query  = sprintf("
+                        UPDATE 
+                            %1\$s 
+                        SET 
+                            permission_level = %2\$d 
+                        WHERE 
+                            id = %3\$d", 
+                        
+                        $mysql_data['db_table_users'],
+                        $mask,
+                        $_GET['id']
+                    );
+                    
+                    $db->query($query);
+                        
+                    $ft->assign('CONFIRM', 'Zwiêkszono poziom uprawnieñ.');
+                }
+            }
+            
+		}
+	
 		$query = sprintf("
             SELECT * FROM 
                 %1\$s 
@@ -208,15 +354,68 @@ switch ($action) {
 		// Pêtla wyswietlaj¹ca wszystkie wpisy + stronnicowanie ich
 		while($db->next_record()) {
 		
-			$user_id = $db->f("id");
-			$login   = $db->f("login");
-			$email   = $db->f("email");
-			$status  = $db->f("active");
+			$user_id     = $db->f("id");
+			$login       = $db->f("login");
+			$email       = $db->f("email");
+			$perm_level  = $db->f("permission_level");
+			$status      = $db->f("active");
+			
+			switch ($perm_level) {
+			    
+			    case '1': $level = 1;
+			    break;
+			    
+			    case '3': $level = 2;
+			    break;
+			    
+			    case '7': $level = 3;
+			    break;
+			    
+			    case '15':$level = 4;
+			    break;
+			    
+			    case '31':$level = 5;
+			    break;
+			   
+			}
+			
+			if($permarr["admin"]){
+			        
+                switch ($level) {
+                    
+                    case '1':
+                    
+                    $ft->assign(array(
+                        'LEVEL_DOWN'    =>' ',
+                        'LEVEL_UP'      =>'<a href="main.php?p=13&amp;plevel=up&amp;id=' . $user_id . '">+</a>'
+                    ));
+                    break;
+                    
+                    case '2':
+                    case '3':
+                    
+                    
+                    $ft->assign(array(
+                        'LEVEL_DOWN'    =>'&nbsp;<a href="main.php?p=13&amp;plevel=down&amp;id=' . $user_id . '">-</a>',
+                        'LEVEL_UP'      =>'<a href="main.php?p=13&amp;plevel=up&amp;id=' . $user_id . '">+</a>'
+                    ));
+                    break;
+                    
+                    case '4':
+                    $ft->assign(array(
+                        'LEVEL_DOWN'    =>'&nbsp;<a href="main.php?p=13&amp;plevel=down&amp;id=' . $user_id . '">-</a>',
+                        'LEVEL_UP'      =>' '
+                    ));
+                    break;
+                }
+			    
+			}
 			
 			$ft->assign(array(
                 'USER_ID'   =>$user_id,
                 'NAME'      =>$login,
-                'EMAIL'     =>$email
+                'EMAIL'     =>$email,
+                'LEVEL'     =>$level,
 			));
 			
 			if($status == 'Y') {
@@ -249,6 +448,7 @@ switch ($action) {
 			}
 		}
 		$ft->parse('ROWS', "editlist_users");
+		$ft->parse('ROWS', ".result_note");
 }
 
 ?>
