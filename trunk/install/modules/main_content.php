4889ca3728ad8187582e22248076d2a87d2e13f6
lark <lark@68905588-48f1-0310-a723-09cfc018eb8a>
2005-04-19 01:58:47 +0000

    Przy dodawaniu pierwszej kategorii wpisow dodajemy wartosc takze do pola category_parent_id

--- trunk/install/modules/main_content.php
+++ trunk/install/modules/main_content.php
@@ -153,7 +153,7 @@ switch ($action) {
                 INSERT INTO
                     %1\$s
                 VALUES
-                    ('', 'ogólna', '')",
+                    ('', 'ogólna', '0', '')",
 
                 $t2
             );
