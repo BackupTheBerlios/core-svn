74e480d8dc4ec77d258a971376e8f7860230ab23
lark <lark@68905588-48f1-0310-a723-09cfc018eb8a>
2005-04-19 01:55:20 +0000

    Rysujemy drzewko jesli sa jakies dzieci danej kategorii

--- trunk/modules/category_list.php
+++ trunk/modules/category_list.php
@@ -2,26 +2,42 @@
 
 $query = sprintf("
     SELECT
-        *
+        category_id, 
+        category_parent_id, 
+        category_name
     FROM 
-        %1\$s",
+        %1\$s 
+    WHERE 
+        category_parent_id = '%2\$d' 
+    ORDER BY 
+        category_id",
 
-    $mysql_data['db_table_category']
+    $mysql_data['db_table_category'],
+    0
 );
 
 $db->query($query);
 
 while($db->next_record()) {
     
+    $cat_id         = $db->f("category_id");
+    $cat_parent_id  = $db->f("category_parent_id");
+    $cat_name       = $db->f("category_name");
+    
     $ft->assign(array(
-        'CAT_NAME'  => str_replace('&', '&amp;', $db->f('category_name')),
-        'NEWS_CAT'  => $db->f('category_id')
+        'CAT_NAME'  =>str_replace('&', '&amp;', $cat_name),
+        'NEWS_CAT'  =>$cat_id, 
+        'CLASS'     =>"cat_parent", 
+        'PARENT'    =>''
     ));
     
     $ft->define("category_list", "category_list.tpl");
     $ft->define_dynamic("category_row", "category_list");
     
     $ft->parse('CATEGORY_LIST', ".category_row");
+    
+    // funkcja pobieraj±ca rekurencyjnie strony dziedzicz±ce::child
+    get_category_cat($cat_id, 2);
     
 }
 
