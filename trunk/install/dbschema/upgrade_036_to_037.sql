fd21399d03d3af79d86057cf0b78d96cd7419a86
lark <lark@68905588-48f1-0310-a723-09cfc018eb8a>
2005-04-24 20:22:00 +0000

    Dodanie nowego pliku sql

---
+++ trunk/install/dbschema/upgrade_036_to_037.sql
@@ -0,0 +1,13 @@
+ALTER TABLE `core_category` 
+    ADD `category_order` MEDIUMINT(7) 
+    DEFAULT '1' NOT NULL AFTER `category_parent_id`;
+    
+UPDATE `core_category` 
+    SET category_order = category_id *10;
+    
+ALTER TABLE `core_pages` 
+    ADD `page_order` MEDIUMINT(7) 
+    DEFAULT '1' NOT NULL AFTER `parent_id`;
+    
+UPDATE `core_pages` 
+    SET page_order = id *10;
