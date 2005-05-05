f7bba4c34f4a5d8fb41d0423ef05a33552e62827
lark <lark@68905588-48f1-0310-a723-09cfc018eb8a>
2005-05-05 16:45:52 +0000

    Pobieramy 5 ostatnich wątków z rss głównej strony core

--- trunk/administration/modules/main.php
+++ trunk/administration/modules/main.php
@@ -1,5 +1,53 @@
 <?php
 
+$file = 'http://core.no1-else.com/rss';       // iSyndicate RSS
+$data = !function_exists('file_get_contents') ? implode('', file($file)) : file_get_contents($file);
+
+$simple = 1;
+
+$replacement = array(
+    "&",
+    "<br />", 
+    "<",
+    ">"
+);
+    
+$pattern = array(
+    " &amp; ",
+    "&lt;br /&gt;",
+    "&lt;",
+    "&gt;"
+);
+    
+$data   = str_replace($pattern, $replacement, $data);
+
+$rss  = new rss_parser($data, $simple);
+
+$allItems   = $rss->getAllItems();
+$itemCount  = count($allItems);
+
+$ft->define("main_site", "main_site.tpl");
+$ft->define_dynamic("rss_row", "main_site");
+
+function str_cut($s, $i=110, $c=' ') {
+    return substr($s, 0, strrpos(substr($s, 0, $i), $c));
+}
+
+for($y = 0; $y < 5; $y++) {
+    
+    
+    
+    $ft->assign(array(
+        'PERMA_LINK'    =>$allItems[$y]['LINK'],
+        'NEWS_TITLE'    =>$allItems[$y]['TITLE'], 
+        'DATE'    =>$allItems[$y]['DATE'],
+        'NEWS_TEXT'     =>str_cut(strip_tags($allItems[$y]['DESCRIPTION'])) . '...'
+    ));
+    
+    $ft->parse('ROWS', ".rss_row");
+    
+}
+
 // Inicjowanie egzemplarza klasy do obs³ugi Bazy Danych
 $db = new DB_SQL;
 
@@ -38,5 +86,5 @@ $ft->assign(array(	'COUNT_NOTES'		=>$num_items,
 					'PUBLISHED_NOTES'	=>$published_items,
 					'NONPUBLISHED_NOTES'=>$nonpublished_items));
 					
-$ft->parse('ROWS', ".main_site");
+$ft->parse('ROWS', "main_site");
 ?>
