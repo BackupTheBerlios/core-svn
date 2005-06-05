ac31cf1028ff21b65e7d9dbb4dd63364dad491aa
mysz <mysz@68905588-48f1-0310-a723-09cfc018eb8a>
2005-06-05 00:41:53 +0000

    - zamiana while() na foreach()

--- trunk/index.php
+++ trunk/index.php
@@ -23,9 +23,9 @@ $rewrite = get_config('mod_rewrite');
 // i w zaleznosci od tego wstawianie addslashes, badz nie.
 if(!get_magic_quotes_gpc()) {
     if(is_array($_GET)) {
-        while(list($k, $v) = each($_GET)) {
+        foreach($_GET as $k => $v) {
             if(is_array($_GET[$k])) {
-                while(list($k2, $v2) = each($_GET[$k])) {
+                foreach ($_GET[$k] as $k2 => $v2) {
                     $_GET[$k][$k2] = addslashes($v2);
                 }
                 @reset($_GET[$k]);
@@ -37,9 +37,9 @@ if(!get_magic_quotes_gpc()) {
     }
     
     if(is_array($_POST)) {
-        while(list($k, $v) = each($_POST)) {
+        foreach ($_POST as $k => $v) {
             if(is_array($_POST[$k])) {
-                while(list($k2, $v2) = each($_POST[$k])) {
+                foreach ($_POST[$k] as $k2 => $v2) {
 					$_POST[$k][$k2] = addslashes($v2);
                 }
                 @reset($_POST[$k]);
@@ -51,9 +51,9 @@ if(!get_magic_quotes_gpc()) {
     }
     
     if(is_array($_COOKIE)) {
-        while(list($k, $v) = each($_COOKIE)) {
+        foreach ($_COOKIE as $k => $v) {
             if(is_array($_COOKIE[$k])) {
-                while(list($k2, $v2) = each($_COOKIE[$k])) {
+                foreach( $_COOKIE[$k] as $k2 => $v2) {
                     $_COOKIE[$k][$k2] = addslashes($v2);
                 }
                 @reset($_COOKIE[$k]);
