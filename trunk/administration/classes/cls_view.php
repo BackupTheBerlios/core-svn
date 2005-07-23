b9de70798efdaec86c59beb700e344344146d50d
lark <lark@68905588-48f1-0310-a723-09cfc018eb8a>
2005-07-23 21:11:41 +0000

    Dodane dwie klasy: - tree() zastepuje funkcje get_cat, get_category_cat i get_breadcrumb. Uzywaja klasy view, ktora pelni role wzorca singleton i inicjuje klase do obslugi bazy danych. Prosze o sprawdzenie kodu. Jesli cos pomieszalem, to wroci sie do funkcji.

---
+++ trunk/administration/classes/cls_view.php
@@ -0,0 +1,30 @@
+<?php
+
+class view {
+    
+    var $db;
+    
+    /*
+     * constructor
+     */
+    function view() {
+        $this->db = new DB_Sql;
+    }
+    
+    
+    /**
+     * @return reference
+     */
+    function &instance() {
+        static $view;
+        
+        if(!isset($view)) {
+            $view = new view;
+        }
+        
+        return $view;
+    }
+    
+    // example
+    // $view = &view::instance();
+}
