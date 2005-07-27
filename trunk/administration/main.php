c184bfaf0e820aac9167a62f4928d8a7a4473ada
mysz <mysz@68905588-48f1-0310-a723-09cfc018eb8a>
2005-07-27 21:48:48 +0000

    - przejscie na nowe stale z config.php - dodane todow kilku miejscach (nie kasuj!, chyba ze zrobisz ;) ) - porzadki i optymalizacja - przejscie na przejscie na sciezki bezwzgledne (to samo dotyczy poprzedniego commita, odnosnie pliku administration/index.php

--- trunk/administration/main.php
+++ trunk/administration/main.php
@@ -1,19 +1,39 @@
 <?php
+/*
+ * IMPORTANT: do not change include to require!
+ *
+ */
+@include_once('inc/config.php');
+if(!defined('CORE_INSTALLED')) {
 
-session_register("login");
-session_register("loggedIn");
+    header('Location: ../install/install.php');
+    exit;
+}
 
-if(!isset($_SESSION["loggedIn"])){
+
+
+
+
+session_register('login');
+session_register('loggedIn');
+
+/*
+ * TODO:
+ * w sesji przechowywaæ login i hash has³a. przy ka¿dym wejœciu
+ * musi byæ sprawdzana poprawnoœæ. inaczej, jeœli ktoœ siê nie 
+ * bêdzie wylogowywa³ wystaczaj¹co d³ugo, mo¿e to spowodowaæ problemy z
+ * bezpieczeñstwem (wy³¹czenie/skasowanie usera nie spowoduje braku mo¿liwoœci
+ * namieszania przez niego w systemie)
+ *
+ */
+if(!isset($_SESSION['loggedIn'])){
 	
-	header("Location: index.php");
+	header('Location: index.php');
 	exit;
 }
 
 // warto¶æ pocz±tkowa zmiennej $start -> potrzebna przy stronnicowaniu
 $start = isset($_GET['start']) ? intval($_GET['start']) : 0;
-
-define('PATH_TO_CLASSES', 'classes');
-define('PATH_TO_MODULES', 'modules');
 
 require_once(PATH_TO_CLASSES. '/cls_db_mysql.php');
 require_once(PATH_TO_CLASSES. '/cls_upload.php');
@@ -21,16 +41,15 @@ require_once(PATH_TO_CLASSES. '/cls_rss_parser.php');
 require_once(PATH_TO_CLASSES. '/cls_links.php');
 require_once(PATH_TO_CLASSES. '/cls_errors.php');
 
-require_once('inc/config.php');
-require_once('../inc/common_lib.php');
-require_once('../inc/admin_lib.php');
+require_once(ROOT . 'inc/common_lib.php');
+require_once(ROOT . 'inc/admin_lib.php');
 
 // mysql_server_version
 get_mysql_server_version();
 
 $lang = get_config('language_set');
 
-require_once('i18n/' . $lang . '/i18n.php');
+require_once(ROOT . 'administration/i18n/' . $lang . '/i18n.php');
 require_once(PATH_TO_CLASSES. '/cls_fast_template.php');
 require_once(PATH_TO_CLASSES. '/cls_permissions.php');
 
@@ -73,9 +92,9 @@ $ft = new FastTemplate('./templates/' . $lang . '/tpl');
 
 // tablica definicji u¿ytych plików *.tpl
 $ft->define(array(
-    'index'         =>"index.tpl",
-    'main_loader'   =>"main_loader.tpl",
-    'result_note'   =>"result_note.tpl"
+    'index'         =>'index.tpl',
+    'main_loader'   =>'main_loader.tpl',
+    'result_note'   =>'result_note.tpl'
 ));
 		
 $ft->assign(array(
@@ -88,20 +107,32 @@ $ft->assign(array(
 ));
 
 $inc_modules = array(
-    "header_menu",
-    "subcat_menu"
+    'header_menu',
+    'subcat_menu'
 );
 
 foreach($inc_modules as $module) {
     
     // ³adowanie dodatkowych modu³ów
-    include('modules/' . $module . '.php');
+    include(PATH_TO_MODULES_ADM . $module . '.php');
 }
 
 // prze³±cznica ³adowanej tre¶ci					
 $p = empty($_GET['p']) ? '' : $_GET['p'];
 
-class loader {
+/*
+ * TODO:
+ * na serio jest to potrzebne ? takie kombinacje ?
+ * nie wystarczylby zwykly pojedynczy switch z jednym ifem ?
+ * wydaje mi sie ze byloby prostsze w obsludze etc...
+ * albo, napisz mi jakie sa zalety, poza zastosowaniem obiektowosci ;)
+ *
+ * a jesli jednak ma byc, to moze nieco zautomatyzowac ? dodac metode 'get'
+ * ktora by sobie grzecznie sama dodawala extensiona do nazwy mmodulu, 
+ * czy jakies takie usprawnienia ?
+ *
+ */
+ class loader {
     
     var $mod = '';
     var $MODULE_EXTENSION = '.php';
@@ -134,18 +165,18 @@ class loader {
         
         $this->mod = $this->name_cleaner($this->mod);
         
-        if($this->mod == "") {
+        if($this->mod == '') {
             $this->return_dead();
         }
 
-		if(!@file_exists(PATH_TO_MODULES . '/' . $this->mod . $this->MODULE_EXTENSION)) {
-			$this->return_dead();
-		}
+      if(!@file_exists(PATH_TO_MODULES_ADM . '/' . $this->mod . $this->MODULE_EXTENSION)) {
+        $this->return_dead();
+      }
     }
     
 	function name_cleaner($name) {
 	    
-	    return preg_replace("/[^a-zA-Z0-9\-\_]/", "", $name);
+	    return preg_replace('/[^a-zA-Z0-9\-\_]/', '', $name);
 	}
 	
 	function return_dead() {
@@ -155,9 +186,9 @@ class loader {
 }
 
 $loader = new loader();
-require_once(PATH_TO_MODULES . '/' . $loader->mod . $loader->MODULE_EXTENSION);
+require_once(PATH_TO_MODULES_ADM . $loader->mod . $loader->MODULE_EXTENSION);
 
-$ft->parse('MAIN_CONTENT', array("main_loader", "index"));
+$ft->parse('MAIN_CONTENT', array('main_loader', 'index'));
 
 $ft->FastPrint();
 exit;
