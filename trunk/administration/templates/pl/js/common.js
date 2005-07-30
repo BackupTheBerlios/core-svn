c25cc2e03aea9704830ef7d49d29b1099bf70280
mysz <mysz@68905588-48f1-0310-a723-09cfc018eb8a>
2005-07-30 16:43:47 +0000

    - dodana funkcja switchChecked() przelaczajaca zaznacozne checkboxy o podanej nazwie\n- dodanea funkcja askChecked() sprawdzajaca czy jakis checkbox o podanej nazwie jest zaznaczony. jesli zaden, to zwraca false, jesli jakikolwiek, to pyta sie (pytanie jako argument) czy na pewno user chce wykoac ta akcje, czy tez nie. zwraca wtedy true lub false. do wykorzystania w sposob: onclick="return askChecked('jestes tego pewien?', 'nazwa_elementu[]')"; dziala takze z buttonami w gecko/msie/opera\n- funkcje form_it() i wywolywanie jej w body onload='' zastaiopno przypisaniem obektu window.onload=fuction(){}; sama funkcja poprawiopna, dzialajaca poprawnie, uwzgledniajaca ilsoc formularzy na stronie etc; nie przenosi takze focusu na elementy zaznaczone jako disabled lub readonly\n- wyrzucono przypisywanie zmeinej edCanvas

--- trunk/administration/templates/pl/js/common.js
+++ trunk/administration/templates/pl/js/common.js
@@ -1,22 +1,59 @@
 function E(id) {
     return document.getElementById(id)
 }
+function switchChecked(v) {
+    
+    a=document.getElementsByName(v)
+    for(i=0; i<a.length; i++) {
+        
+        if (a[i].checked) {a[i].checked = ''}
+        else {a[i].checked = 'true' }
+    }
+}
+function askChecked(q, v)
+{
+    a = document.getElementsByName(v)
+    c = false
+    for (i=0; i<a.length; i++) {
+        if (a[i].checked) {
+            c = true
+            break
+        }
+    }
+    return c ? confirm(q) : false
+}
 
-function form_it() {
+window.onload = function(){
     forms = document.getElementsByTagName('form')
+
     if (forms.length > 0) {
-        f = forms[0]
-        var lp=0
-        //for (i in f) {alert(i.tagName); if (lp++ > 4) break}
-        for (i=0; i<f.length; i++)
+        for (var k=0; k<forms.length; k++)
         {
-            t = f[i].type
-            n = f[i].tagName
-            if ( ( n == 'input' && ( t == 'text' || t == 'password' || t == 'checkbox' || t == 'radio')) || n == 'select') {
-                f[i].focus()
-                break
+            f = forms[k]
+            for (var i=0; i<f.elements.length; i++)
+            {
+                e = f[i]
+                n = ('tagName' in e) ? e.tagName.toLowerCase() : ''
+                t = ('type' in e) ? e.type.toLowerCase() : ''
+                d = ('disabled' in e) ? e.disabled : false
+                r = ('readOnly' in e) ? e.readOnly : false
+                if ( ( ( n == 'input' && ( t == 'text' || t == 'password' || t == 'checkbox' || t == 'radio') ) 
+                        || n == 'select' || n == 'textarea') && d == false && r == false){
+                    e.focus()
+                    k=forms.length + 1
+                    break
+                }
             }
         }
     }
+    
+    edCanvas = E('canvas')
+}
+function toggleDisable(id) {
+    e = E(id)
+    if (e) e.disabled = e.disabled ? '' : 'true'
 }
 
+function dw(s) {
+    document.write(s + '<br />')
+}
