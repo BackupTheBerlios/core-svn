9d1b0f4384a05b248807d449a5582b2c01fe1689
mysz <mysz@68905588-48f1-0310-a723-09cfc018eb8a>
2005-08-03 21:30:59 +0000

    - dodane funkcje: tpl_categories() i tpl_categories_tree(). tpl_categories_tree() jest wywolywane z poziomu tpl_categories() i razem wparsowuja do podanego w parametrze taga drzewko kategorii wpisow

--- trunk/administration/inc/tpl_functions.php
+++ trunk/administration/inc/tpl_functions.php
@@ -20,5 +20,39 @@ function tpl_message($msg, $msg_intro = '', $parse_tag = 'MESSAGE') {
 
     return true;
 }
+
+function tpl_categories($tag = 'CATEGORIES', &$cat_array, $parent = 0, $selected = array()) {
+    global $ft;
+
+    $ft->define(array('categories' => 'categories.tpl'));
+
+    $ft->define_dynamic('cat_row', 'categories');
+
+    tpl_categories_tree($cat_array, $parent, $selected);
+    
+    $ft->parse($tag, '.categories');
+    
+}
+function tpl_categories_tree(&$cat_array, $parent = 0, $selected = array()) {
+    if ( !array_key_exists($parent, $cat_array) ) return false;
+
+    global $ft;
+    static $pad = 0;
+    
+    foreach ($cat_array[$parent] as $c_id => $c_name) {
+        $ft->assign(array(
+            'C_ID'          => $c_id,
+            'C_NAME'        => $c_name,
+            'CURRENT_CAT'   => in_array($c_id, $selected) ? 'checked="checked"' : '',
+            'PAD'           => $pad
+        ));
+        $ft->parse('CAT_ROW', '.cat_row');
+
+        $pad+=15;
+        tpl_categories_tree($cat_array, $c_id, $selected);
+        $pad-=15;
+    }
+}
+
     
 ?>
