5fb9700e6fbc59dc889c10d0f2357400d14ec6ed
lark <lark@68905588-48f1-0310-a723-09cfc018eb8a>
2005-10-22 23:49:55 +0000

    Comments added

--- branches/050/administration/classes/cls_corebase.php
+++ branches/050/administration/classes/cls_corebase.php
@@ -1,52 +1,60 @@
 <?php
 
-class CoreBase
-{
+class CoreBase {
+    
     var $error = array();
     var $debug = false;
 
-    function CoreBase()                                            //KONSTRUKTOR
-    {
+    /*
+     * constructor
+     */
+    function CoreBase() {
+        
         global $i18n;
 
-        $view =& view::instance();
-        $this->db =& $view->db;
-
+        $view       =& view::instance();
+        $this->db   =& $view->db;
         $this->i18n =& $i18n;
     }
+    
 
-    function error_set($msg)
-    {
-        if (is_array($msg))
-        {
-            $this->error = array_merge($this->error, $msg);
-        }
-        else
-        {
-            $this->error[] = (string)$msg;
-        }
+    /**
+     * @param $msg
+     * @return error array
+     */
+    function error_set($msg) {
 
+        is_array($msg) ? $this->error = array_merge($this->error, $msg) : $this->error[] = (string)$msg;
         return true;
     }
-    function error_get($last = false)
-    {
-        if ((bool)count($this->error))
-        {
-            if ($last)
-            {
+    
+    /**
+     * @param $last - error msg
+     * @return array
+     */
+    function error_get($last = false) {
+        if((bool)count($this->error)) {
+            if($last) {
                 return end($this->error);
             }
             return $this->error;
         }
         return array();
     }
-    function error_clear()
-    {
+    
+    /**
+     * @return array
+     */
+    function error_clear() {
         $this->error = array();
         return true;
     }
-    function is_error()
-    {
+    
+    
+    /**
+     * @return number of errors
+     */
+    function is_error() {
         return (bool)count($this->error);
     }
 }
