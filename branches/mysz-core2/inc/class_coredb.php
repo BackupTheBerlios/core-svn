c1507acf7cb8ab77484bb3a32e48780725c85f47
lark <lark@68905588-48f1-0310-a723-09cfc018eb8a>
2006-02-15 01:14:30 +0000

    tab size changed s/2/4

--- branches/mysz-core2/inc/class_coredb.php
+++ branches/mysz-core2/inc/class_coredb.php
@@ -2,36 +2,41 @@
 // $Id$
 
 class CoreDB {
-  private static $instance = null;
-
-  private function __construct() {}
-
-  public static function connect($type='mysql') {
-    if (!isset(self::$instance)) {
-      $c = __CLASS__;
-      self::$instance = new $c;
-    }
-    $conn = null;
-    try {
-      switch ($type) {
-        case 'mysql':
-          $conn =& new PDO(sprintf('mysql:host=%s;dbname=%s', DB_HOST, DB_NAME), DB_USER, DB_PASS);
-          break;
-        default:
-          throw new CESyntaxError('Invalid database type.');
-      }
-    } catch (PDOException $e) {
-      throw new CEDBError(sprintf('Connection failed: %s.', $e->getMessage()));
-    }
+    private static $instance = null;
+    
+    private function __construct() {}
+    
+    public static function connect($type='mysql') {
+        if(!isset(self::$instance)) {
+            $c = __CLASS__;
+            self::$instance = new $c;
+        }
         
-    self::$instance->db = $conn;
-
-    return self::$instance->db;
-  }
-  
-  public function __clone() {
-    throw new CESyntaxError('Clone not allowed.');
-  }
+        $conn = null;
+        
+        try {
+            switch($type) {
+                case 'mysql':
+                    $conn =& new PDO(sprintf(
+                        'mysql:host=%s;dbname=%s', DB_HOST, DB_NAME), 
+                        DB_USER, 
+                        DB_PASS
+                    );
+                break;
+            default:
+                throw new CESyntaxError('Invalid database type.');
+            }
+        } catch(PDOException $e) {
+            throw new CEDBError(sprintf('Connection failed: %s.', $e->getMessage()));
+        }
+        
+        self::$instance->db = $conn;
+        return self::$instance->db;
+    }
+    
+    public function __clone() {
+        throw new CESyntaxError('Clone not allowed.');
+    }
 }
 
 // vim: expandtab:shiftwidth=4:softtabstop=4:tabstop=4
