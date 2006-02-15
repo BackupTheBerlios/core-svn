36408c31af8a8140c1ebf286bcbb147f698f03f3
mysz <mysz@68905588-48f1-0310-a723-09cfc018eb8a>
2006-02-15 02:39:11 +0000

    - phpdoc comments

--- branches/mysz-core2/inc/class_coredb.php
+++ branches/mysz-core2/inc/class_coredb.php
@@ -1,12 +1,81 @@
 <?php
-// $Id$
 
+// vim: expandtab shiftwidth=4 softtabstop=4 tabstop=4
+
+/**
+ * Provide class for database connection as a singletone
+ *
+ * PHP versions 4 and 5
+ *
+ * This file is internal part of Core CMS (http://core-cms.com/) engine.
+ *
+ * Copyright (C) 2004-2005 Core Dev Team (more info: docs/AUTHORS).
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation; version 2 only.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * @category   Classes
+ * @package    Classes
+ * @author     Core Dev Team <core@core-cms.com>
+ * @copyright  2006 Core Dev Team
+ * @license    http://www.fsf.org/copyleft/gpl.html
+ * @license    http://www.gnu.org.pl/text/licencja-gnu.html
+ * @version    SVN: $Id:$
+ * @link       http://core-cms.com/
+ */
+
+/**
+ * Singleton's pattern of database connection class
+ *
+ * @category   Classes
+ * @package    Classess
+ * @author     Core Dev Team <core@core-cms.com>
+ * @copyright  2006 Core Dev Team
+ * @license    http://www.fsf.org/copyleft/gpl.html
+ * @license    http://www.gnu.org.pl/text/licencja-gnu.html
+ * @version    SVN: $Id:$
+ * @link       http://core-cms.com/
+ */
 class CoreDB {
     
+    /**
+     * Instance of PDO connection
+     *
+     * @var object
+     * @access private
+     * @static
+     */
     private static $_instance = null;
     
+    /**
+     * Constructor
+     *
+     * Private, without parameters and body.
+     *
+     * @access private
+     */
     private function __construct() {}
-    
+
+    /**
+     * Connecting with database.
+     *
+     * Function create an singleton object, and creates database connection.
+     * Connection handler is stored in self::$_instance->db variable.
+     *
+     * @param string $type database type. Only 'mysql' like for now.
+     * @return object PDO
+     * @throws CESyntaxError if incorrect database type
+     * @throws CEDBError     if problem with connect with database
+     *
+     * @access public
+     * @static
+     */
     public static function connect($type='mysql')
     {
         if(!isset(self::$_instance)) {
@@ -34,13 +103,18 @@ class CoreDB {
         
         return self::$_instance->db;
     }
-    
+
+    /**
+     * Overloaded method to forbid cloning of instance
+     *
+     * @throws CESyntaxError always when invoked
+     * @access public
+     */
     public function __clone()
     {
         throw new CESyntaxError('Clone not allowed.');
     }
 }
 
-// vim: expandtab:shiftwidth=4:softtabstop=4:tabstop=4
 ?>
 
