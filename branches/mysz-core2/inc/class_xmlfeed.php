b1d917cf46e7c3ded91cace139a9f94ad822555f
lark <lark@68905588-48f1-0310-a723-09cfc018eb8a>
2006-02-26 12:54:30 +0000

    Added prepare_xml() method - input as an array - output xml feed. Added example array of news content NFY

--- branches/mysz-core2/inc/class_xmlfeed.php
+++ branches/mysz-core2/inc/class_xmlfeed.php
@@ -30,32 +30,57 @@
  * @link       http://core-cms.com/
  */
 
+
+/**
+ * Class for prepare & parse xml feed, based on DOM
+ *
+ * Sets basic settings of xml file, like headers. Prepare data from array to
+ * parse feed.
+ *
+ * @category   Classes
+ * @package    Classes
+ * @author     Core Dev Team <core@core-cms.com>
+ * @copyright  2006 Core Dev Team
+ * @license    http://www.fsf.org/copyleft/gpl.html
+ * @license    http://www.gnu.org.pl/text/licencja-gnu.html
+ * @version    SVN: $Id$
+ * @link       http://core-cms.com/
+ */
 class XmlFeed extends DOMDocument {
 
     /**
-     * elements - data content array
+     * settings - basic settings array
      *
      * @var array
      * @access protected
      */
-    protected $elements = array(
-        'title'     =>'some title',
-        'link'      =>'http://somesite.com',
-        'copyright' =>'Core Dev Team'
+    protected $settings = array(
+        'title'         => 'Core CMS - RSS Feed',
+        'link'          => 'http://core-cms.com',
+        'description'   => '',
+        'language'      => 'pl',
+        'copyright'     => '2006 Core Dev Team',
+        'docs'          => 'http://blogs.law.harvard.edu/tech/rss',
+        'webMaster'     => 'core@core-cms.com',
+        'lastBuildDate' => 'Sun, 26 Feb 2006 13:07:07 EST' // example
     );
 
+    
     /**
      * Constructor
      *
-     * Initialize new DomDocument. Set the basic data, like xml version and encoding
+     * Initialize new DomDocument. Set the basic data, like xml version and 
+     * encoding
      *
      * @access public
      */
-    public function __construct()
-    {
-        parent::__construct('1.0', 'utf-8');
+    public function __construct($version, $encoding)
+    {   
+        parent::__construct($version, $encoding);
+        $this->set_xml();
     }
 
+    
     /**
      * Set the basic data, like rss version, feed title, copyrights.
      *
@@ -69,29 +94,67 @@ class XmlFeed extends DOMDocument {
 
         $this->root->setAttribute('version', '2.0');
 
-        // create channel element
+        // create the channel element
         $channel = $this->createElement('channel');
         $channel = $this->root->appendChild($channel);
 
-        foreach($this->elements as $name => $value) {
+        foreach($this->settings as $name => $value) {
             $name = $this->createElement($name);
             $name = $channel->appendChild($name);
 
             $name->appendChild($this->createTextNode($value));
         }
 
-        // cleans array, will be used later for content data
-        unset($this->elements);
-
-        $this->parse_feed();
+        // cleans array
+        unset($this->settings);
+        
+    }
+    
+    
+    /**
+     * Set properties of feed from an array.
+     *
+     * @param $content array of properties
+     *
+     * @throws CESyntaxError if gettype($array) != array
+     *
+     * @access public
+     */
+    public function prepare_xml($content)
+    {
+        if (!is_array($content)) {
+            throw new CESyntaxError(sprintf(
+                'Incorrect argument type: expected "array", received "%s".',
+                gettype($content)
+            ));
+        }
+        
+        foreach($content as $element) {
+            
+            // create the item element
+            $item = $this->createElement("item");
+            $item = $this->root->appendChild($item);
+            
+            foreach($element as $name=>$value) {
+                
+                $name = $this->createElement($name);
+                $name = $item->appendChild($name);
+                $name->appendChild($this->createTextNode($value));
+            }
+            unset($element);
+        }
+        unset($content);
+        
+        $this->parse_xml();
     }
 
+    
     /**
      * Send headers and output feed to the browser
      *
      * @access public
      */
-    public function parse_feed()
+    public function parse_xml()
     {
         header('Content-Type: text/xml');
         echo $this->saveXML();
@@ -99,7 +162,14 @@ class XmlFeed extends DOMDocument {
 }
 
 // returns feed
-// $a = new XmlFeed();
-// $a->set_xml();
+$content[0]["title"] = "Some title";
+$content[0]["description"] = "Current news content";
+$content[0]["link"] = "http://core-cms.com/";
+$content[0]["author"] = "Core Dev Team";
+$content[0]["pubDate"] = "Sun, 26 Feb 2006 13:07:07 EST"; // example
+$content[0]["category"] = "main";
+
+$a = new XmlFeed('1.0', 'utf-8');
+$a->prepare_xml($content);
 
 ?>
