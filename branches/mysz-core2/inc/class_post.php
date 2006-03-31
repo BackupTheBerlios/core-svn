05b2345e8e95ce744c17c012d8056ab253846e11
mysz <mysz@68905588-48f1-0310-a723-09cfc018eb8a>
2006-03-31 17:43:03 +0000

    - added phpdoc comments - removed constantd ORDER_*, moved as DESC,ASC to CoreBase - new constants for post status: DRAFT, PUBLISHED and DISABLED - using CEReadOnly - new method Post::set_status() (moved from CorePost) as external setter for status - improved Post::setFromDB() - removed Post::show() (is inherited from CoreBase) - Post::save() has now proper body

--- branches/mysz-core2/inc/class_post.php
+++ branches/mysz-core2/inc/class_post.php
@@ -1,32 +1,163 @@
 <?php
 
 // vim: expandtab shiftwidth=4 softtabstop=4 tabstop=4
-// $Id: index.php 1303 2006-03-08 20:31:39Z mysz $
-// $HeadURL: svn://svn.berlios.de/core/branches/mysz-core2/index.php $
 
+/**
+ * Class for operations on posts
+ *
+ * PHP version 5
+ *
+ * This file is internal part of Core CMS (http://core-cms.com/) engine.
+ *
+ * Copyright (C) 2004-2005 Core Dev Team (more info: docs/AUTHORS).
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation; version 2 only.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * @category   Classes
+ * @package    Classes
+ * @author     Core Dev Team <core@core-cms.com>
+ * @copyright  2006 Core Dev Team
+ * @license    http://www.fsf.org/copyleft/gpl.html
+ * @license    http://www.gnu.org.pl/text/licencja-gnu.html
+ * @version    SVN: $Id: class_image.php 1299 2006-03-08 19:59:18Z mysz $
+ * @link       $HeadURL: svn://svn.berlios.de/core/branches/mysz-core2/inc/class_image.php $
+ */
+
+/**
+ * Class for operations on posts
+ *
+ * Error codes:
+ *  10 - CEReadOnly
+ * 100 - CESyntaxError      Category|Group name cannot be changed from Post object.
+ * 500 - CEDBError
+ *
+ * @category   Classes
+ * @package    Classes
+ * @author     Core Dev Team <core@core-cms.com>
+ * @copyright  2006 Core Dev Team
+ * @license    http://www.fsf.org/copyleft/gpl.html
+ * @license    http://www.gnu.org.pl/text/licencja-gnu.html
+ * @version    SVN: $Id: class_image.php 1299 2006-03-08 19:59:18Z mysz $
+ * @link       $HeadURL: svn://svn.berlios.de/core/branches/mysz-core2/inc/class_image.php $
+ */
 final class Post extends CorePost
 {
-    const ORDER_DESC = 'DESC';
-    const ORDER_ASC  = 'ASC';
+    /**
+     * Constant
+     *
+     * Literal 'published' for use as status
+     */
+    const PUBLISHED = 'published';
 
+    /**
+     * Constant
+     *
+     * Literal 'draft' for use as status
+     */
+    const DRAFT     = 'draft';
+
+    /**
+     * Constant
+     *
+     * Literal 'disabled' for use as status
+     */
+    const DISABLED  = 'disabled';
+
+    /**
+     * Available values for entry status
+     *
+     * @var array
+     *
+     * @access protected
+     * @static
+     */
+    protected static $status_array = array(self::PUBLISHED, self::DRAFT, self::DISABLED);
+
+    /**
+     * Post properties
+     * 
+     * More info: {@link CoreBase::$properties} and {@link CoreBase::$base_properties}.
+     *
+     * @var array
+     * @access protected
+     * @static
+     */
     protected static $base_properties = array(
         'sticky'    => array(null, 'boolean'),
-        'cat_name'  => array('', 'string'),
-        'grp_name'  => array('', 'string')
+        'cat_name'  => array(null, 'string'),
+        'grp_name'  => array(null, 'string'),
+        'id_group'  => array(null, 'integer'),
     );
 
+    /**
+     * List of external setters
+     * 
+     * More info: {@link CoreBase::$setExternal} and {@link CoreBase::$base_setExternal}.
+     *
+     * @var array
+     * @access protected
+     * @static
+     */
     protected static $base_setExternal = array('cat_name', 'grp_name');
 
+    /**
+     * List of external getters
+     * 
+     * More info: {@link CoreBase::$getExternal} and {@link CoreBase::$base_getExternal}.
+     *
+     * @var array
+     * @access protected
+     * @static
+     */
     protected static $base_getExternal = array();
 
-    protected $properties; //  = array();
-    protected $getExternal = array();
-    protected $setExternal = array();
-    
+    /**
+     * All post properties together
+     *
+     * @var array
+     * @access protected
+     */
+    protected $properties;
 
-    public function __construct(&$data)
+    /**
+     * All post external getters together
+     *
+     * @var array
+     * @access protected
+     */
+    protected $getExternal;
+
+    /**
+     * All post external setters together
+     *
+     * @var array
+     * @access protected
+     */
+    protected $setExternal;
+
+    /**
+     * Constructor
+     *
+     * Merge some internal arrays (look for {@link CorePost::__construct} docs)
+     * and set Post properties from arrays or database.
+     *
+     * If $data is integer, it will execute {@link Post::setFromDB()} method.
+     * Else, if $data is an array, it will be passed to {@link Post::setFromArray()}.
+     *
+     * @param mixed $data
+     *
+     * @access public
+     */
+    public function __construct(&$data=null)
     {
-        parent::__construct($data);
+        parent::__construct();
 
         $this->properties = array_merge(
             parent::$base_properties,
@@ -51,25 +182,72 @@ final class Post extends CorePost
         }
     }
 
-    private function set_cat_name($name)
+    /**
+     * External setter for category name - prevents from set category name here
+     *
+     * @param string $name
+     *
+     * @throws CEReadOnly
+     *
+     * @access protected
+     */
+    protected function set_cat_name($name)
     {
-        throw new CESyntaxError('Category name cannot be change from Post object.');
+        throw new CEReadOnly('Read only property.');
     }
     
-    private function set_grp_name($name)
+    /**
+     * External setter for group name - prevents from set category name here
+     *
+     * @param string $name
+     *
+     * @throws CEReadOnly
+     *
+     * @access protected
+     */
+    protected function set_grp_name($name)
     {
-        throw new CESyntaxError('Category name cannot be change from Post object.');
+        throw new CEReadOnly('Read only property.');
     }
 
-    public function save()
+    /**
+     * Checks for correct value of status
+     *
+     * Proper values:
+     *   - {@link Post::PUBLISHED}
+     *   - {@link Post::DRAFT}
+     *   - {@link Post::DISABLED}
+     *
+     * @param string $data status
+     *
+     * @return boolean
+     * @throws CESyntaxError if incorrect type ({@link CoreBase::isType()})
+     *
+     * @access protected
+     */
+    protected function set_status(&$data)
     {
-    }
-    public function setFromDB($id, $order=self::ORDER_DESC, $where=null)
-    {
-        if (!in_array($order, array(self::ORDER_DESC, self::ORDER_ASC))) {
-            $order = self::ORDER_DESC;
+        $this->isType('status', $data);
+
+        if (!in_array($data, self::$status_array)) {
+            $this->errorSet(sprintf('Incorrect status "%s".', $data));
+            return false;
         }
+        $this->properties['status'][0] = $data;
+    }
 
+    /**
+     * Get post data from database
+     * 
+     * @param integer $id post id
+     *
+     * @return boolean
+     * @throws CEDBError
+     *
+     * @access public
+     */
+    public function setFromDB($id)
+    {
         $query = sprintf("
             SELECT
                 posts.id_post           AS id_post,
@@ -84,7 +262,7 @@ final class Post extends CorePost
                 posts.tpl_name          AS tpl_name,
                 posts.author_name       AS author_name,
                 posts.author_mail       AS author_mail,
-                posts.author_www        AS auhtor_www,
+                posts.author_www        AS author_www,
                 posts.date_add          AS date_add,
                 posts.date_mod          AS date_mod,
                 posts.status            AS status,
@@ -110,12 +288,124 @@ final class Post extends CorePost
             TBL_POSTGROUPS,
             $id
         );
+
+        try {
+            $stmt = $this->db->query($query);
+            $result = $stmt->fetch(PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            throw new CEDBError($e->getMessage(), 500);
+        }
+        
+        $result['id_post']    = (int)$result['id_post'];
+        $result['id_parent']  = (int)$result['id_parent'];
+        $result['id_cat']     = (int)$result['id_cat'];
+        $result['id_group']   = (int)$result['id_group'];
+        $result['id_section'] = (int)$result['id_section'];
+
+        return $this->setFromArray($result);
     }
 
-    public function show() {
-        $it = $this->getIter();
-        foreach ($it as $k=>$prop) {
-            printf('<b>%s</b>: %s<br />', $k, $prop);
+
+    /**
+     * Save post data in database
+     *
+     * On success, if we set insert new post, it will return ID of this
+     * entry. Otherwise, return true.
+     *
+     * @return mixed
+     * @throws CEDBError ({@link CEDBError description})
+     *
+     * @access public
+     */
+    public function save()
+    {
+        if (isset($this->id_post)) {
+            $query = sprintf("
+                UPDATE
+                    %s
+                SET
+                    id_parent    = %d,
+                    id_cat       = %d,
+                    id_group     = %d,
+                    id_section   = %d,
+                    title        = %s,
+                    permalink    = %s,
+                    caption      = %s,
+                    body         = %s,
+                    tpl_name     = %s,
+                    author_name  = %s,
+                    author_mail  = %s,
+                    author_www   = %s,
+                    date_mod     = NOW(),
+                    status       = %s
+                WHERE   
+                    id_post      = %d",
+                
+                TBL_POSTS,
+                $this->id_parent,
+                $this->id_cat,
+                $this->id_group,
+                $this->id_section,
+                $this->quotenull($this->title),
+                $this->quotenull($this->permalink),
+                $this->quotenull($this->caption),
+                $this->quotenull($this->body),
+                $this->quotenull($this->tpl_name),
+                $this->quotenull($this->author_name),
+                $this->quotenull($this->author_mail),
+                $this->quotenull($this->author_www),
+                $this->quotenull($this->status),
+                $this->id_post
+            );
+        } else {
+            $query = sprintf("
+                INSERT INTO
+                    %s
+                SET
+                    id_parent    = %d,
+                    id_cat       = %d,
+                    id_group     = %d,
+                    id_section   = %d,
+                    title        = %s,
+                    permalink    = %s,
+                    caption      = %s,
+                    body         = %s,
+                    tpl_name     = %s,
+                    author_name  = %s,
+                    author_mail  = %s,
+                    author_www   = %s,
+                    date_add     = NOW(),
+                    date_mod     = NOW(),
+                    status       = %s",
+                
+                TBL_POSTS,
+                $this->id_parent,
+                $this->id_cat,
+                $this->id_group,
+                $this->id_section,
+                $this->quotenull($this->title),
+                $this->quotenull($this->permalink),
+                $this->quotenull($this->caption),
+                $this->quotenull($this->body),
+                $this->quotenull($this->tpl_name),
+                $this->quotenull($this->author_name),
+                $this->quotenull($this->author_mail),
+                $this->quotenull($this->author_www),
+                $this->quotenull($this->status)
+            );
+        }
+
+        try {
+            $this->db->exec($query);
+        } catch (PDOException $e) {
+            throw new CEDBError($e->getMessage(), 500);
+        }
+
+        if (isset($this->id_post)) {
+            return true;
+        } else {
+            $this->id_post = (int)$this->db->lastInsertId();
+            return $this->id_post;
         }
     }
 }
