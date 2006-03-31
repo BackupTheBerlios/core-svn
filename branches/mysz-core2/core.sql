60d9168f32d301d528ad152380d8fc8f9e644826
mysz <mysz@68905588-48f1-0310-a723-09cfc018eb8a>
2006-03-31 17:08:12 +0000

    - update

--- branches/mysz-core2/core.sql
+++ branches/mysz-core2/core.sql
@@ -1,17 +1,9 @@
-<span style='color:#ff0000'><br />
-<b>Strict Standards</b>:  var: Deprecated. Please use the public/private/protected modifiers in <b>C:\www\htdocs\mysql\libraries\zip.lib.php</b> on line <b>32</b><br />
-</span><span style='color:#ff0000'><br />
-<b>Strict Standards</b>:  var: Deprecated. Please use the public/private/protected modifiers in <b>C:\www\htdocs\mysql\libraries\zip.lib.php</b> on line <b>39</b><br />
-</span><span style='color:#ff0000'><br />
-<b>Strict Standards</b>:  var: Deprecated. Please use the public/private/protected modifiers in <b>C:\www\htdocs\mysql\libraries\zip.lib.php</b> on line <b>46</b><br />
-</span><span style='color:#ff0000'><br />
-<b>Strict Standards</b>:  var: Deprecated. Please use the public/private/protected modifiers in <b>C:\www\htdocs\mysql\libraries\zip.lib.php</b> on line <b>53</b><br />
-</span>-- phpMyAdmin SQL Dump
+-- phpMyAdmin SQL Dump
 -- version 2.6.3-pl1
 -- http://www.phpmyadmin.net
 -- 
 -- Host: localhost
--- Czas wygenerowania: 13 Mar 2006, 14:43
+-- Czas wygenerowania: 31 Mar 2006, 15:10
 -- Wersja serwera: 5.0.15
 -- Wersja PHP: 5.1.2
 -- 
@@ -24,11 +16,12 @@
 -- Struktura tabeli dla  `core_config`
 -- 
 
+DROP TABLE IF EXISTS `core_config`;
 CREATE TABLE `core_config` (
   `key` varchar(255) NOT NULL default '',
   `value` longtext NOT NULL,
   PRIMARY KEY  (`key`)
-) TYPE=MyISAM;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- 
 -- Zrzut danych tabeli `core_config`
@@ -45,12 +38,13 @@ INSERT INTO `core_config` VALUES ('comp_level', 'i:5;');
 -- Struktura tabeli dla  `core_menusection`
 -- 
 
+DROP TABLE IF EXISTS `core_menusection`;
 CREATE TABLE `core_menusection` (
   `id_menu` bigint(20) unsigned NOT NULL,
   `title` varchar(255) NOT NULL default '',
   `listed_as_tree` char(1) NOT NULL default '0',
   PRIMARY KEY  (`id_menu`)
-) TYPE=MyISAM;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- 
 -- Zrzut danych tabeli `core_menusection`
@@ -63,12 +57,13 @@ CREATE TABLE `core_menusection` (
 -- Struktura tabeli dla  `core_p2c`
 -- 
 
+DROP TABLE IF EXISTS `core_p2c`;
 CREATE TABLE `core_p2c` (
   `id_p2c` bigint(20) unsigned NOT NULL,
   `id_cat` bigint(20) unsigned NOT NULL,
   `id_post` bigint(20) unsigned NOT NULL,
   PRIMARY KEY  (`id_p2c`)
-) TYPE=MyISAM;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- 
 -- Zrzut danych tabeli `core_p2c`
@@ -81,22 +76,23 @@ CREATE TABLE `core_p2c` (
 -- Struktura tabeli dla  `core_postcats`
 -- 
 
+DROP TABLE IF EXISTS `core_postcats`;
 CREATE TABLE `core_postcats` (
-  `id_cat` bigint(20) unsigned NOT NULL,
+  `id_cat` bigint(20) unsigned NOT NULL auto_increment,
   `id_parent` bigint(20) unsigned NOT NULL,
   `name` varchar(255) NOT NULL,
   `permalink` varchar(255) NOT NULL,
-  `description` text NOT NULL,
-  `tpl_name` varchar(255) NOT NULL,
+  `description` text,
+  `tpl_name` varchar(255) default NULL,
   `enabled` tinyint(1) NOT NULL default '1',
   PRIMARY KEY  (`id_cat`)
-) TYPE=MyISAM AUTO_INCREMENT=3 ;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 -- 
 -- Zrzut danych tabeli `core_postcats`
 -- 
 
-INSERT INTO `core_postcats` VALUES (1, 0, 'kat1', 'kat1', '', '', 1);
+INSERT INTO `core_postcats` VALUES (1, 0, 'kat1', 'kat1', 'desc', '', -1);
 INSERT INTO `core_postcats` VALUES (2, 0, 'kat2', 'kat2', '', '', 1);
 
 -- --------------------------------------------------------
@@ -105,13 +101,14 @@ INSERT INTO `core_postcats` VALUES (2, 0, 'kat2', 'kat2', '', '', 1);
 -- Struktura tabeli dla  `core_postgroups`
 -- 
 
+DROP TABLE IF EXISTS `core_postgroups`;
 CREATE TABLE `core_postgroups` (
-  `id_group` bigint(20) unsigned NOT NULL,
+  `id_group` bigint(20) unsigned NOT NULL auto_increment,
   `grp_name` varchar(255) NOT NULL,
   `tpl_name` varchar(255) NOT NULL,
   `enabled` tinyint(1) NOT NULL default '1',
   PRIMARY KEY  (`id_group`)
-) TYPE=MyISAM AUTO_INCREMENT=4 ;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 -- 
 -- Zrzut danych tabeli `core_postgroups`
@@ -127,13 +124,14 @@ INSERT INTO `core_postgroups` VALUES (3, 'page', '', 1);
 -- Struktura tabeli dla  `core_postmeta`
 -- 
 
+DROP TABLE IF EXISTS `core_postmeta`;
 CREATE TABLE `core_postmeta` (
   `id_meta` bigint(20) unsigned NOT NULL,
   `id_post` bigint(20) unsigned NOT NULL,
   `key` varchar(255) NOT NULL,
   `value` longtext NOT NULL,
   PRIMARY KEY  (`id_meta`)
-) TYPE=MyISAM;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- 
 -- Zrzut danych tabeli `core_postmeta`
@@ -146,8 +144,9 @@ CREATE TABLE `core_postmeta` (
 -- Struktura tabeli dla  `core_posts`
 -- 
 
+DROP TABLE IF EXISTS `core_posts`;
 CREATE TABLE `core_posts` (
-  `id_post` bigint(20) unsigned NOT NULL,
+  `id_post` bigint(20) unsigned NOT NULL auto_increment,
   `id_parent` bigint(20) unsigned NOT NULL,
   `id_cat` bigint(20) unsigned NOT NULL,
   `id_group` bigint(20) unsigned NOT NULL,
@@ -164,15 +163,15 @@ CREATE TABLE `core_posts` (
   `date_mod` datetime NOT NULL,
   `status` enum('published','draft','disabled') NOT NULL default 'published',
   PRIMARY KEY  (`id_post`)
-) TYPE=MyISAM AUTO_INCREMENT=4 ;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 -- 
 -- Zrzut danych tabeli `core_posts`
 -- 
 
-INSERT INTO `core_posts` VALUES (1, 0, 1, 1, 0, 'post1', 'post1', 'siakis caption post 1', 'siakis text post 1', NULL, 'mysz', 'marcin@urzenia.net', 'http://diary.urzenia.net/', '2006-03-13 11:51:38', '2006-03-13 11:51:38', 'published');
-INSERT INTO `core_posts` VALUES (2, 0, 2, 1, 0, 'post1', 'post1', 'siakis caption post 1', 'siakis text post 1', NULL, 'mysz', 'marcin@urzenia.net', 'http://diary.urzenia.net/', '2006-03-13 11:51:57', '2006-03-13 11:51:57', 'published');
-INSERT INTO `core_posts` VALUES (3, 0, 1, 1, 0, 'post2', 'post2', 'siakis caption post2', 'siakis text post 2', NULL, 'ania', 'ania@wp.pl', 'http://wp.pl', '2006-03-13 11:51:57', '2006-03-13 11:51:57', 'published');
+INSERT INTO `core_posts` VALUES (1, 0, 1, 1, 0, 'asd', 'post1', 'siakis caption post 1', 'siakis text post 1', 'specjal.tpl', 'mysz', 'marcin@urzenia.net', 'http://diary.urzenia.net/', '2006-03-13 11:51:38', '2006-03-30 13:30:38', 'published');
+INSERT INTO `core_posts` VALUES (2, 0, 1, 1, 0, 'specjal title', 'post1', 'specjal caption', 'specjal body', NULL, 'stasiu', 'marcin@urzenia.net', NULL, '2006-03-31 08:03:35', '2006-03-31 12:32:40', 'published');
+INSERT INTO `core_posts` VALUES (3, 0, 1, 1, 0, 'specjal title', 'post1', 'specjal caption', 'specjal body', NULL, 'stasiu', 'marcin@urzenia.net', 'http://sztolcman.eu', '2006-03-31 12:33:21', '2006-03-31 12:33:21', 'published');
 
 -- --------------------------------------------------------
 
@@ -180,6 +179,7 @@ INSERT INTO `core_posts` VALUES (3, 0, 1, 1, 0, 'post2', 'post2', 'siakis captio
 -- Struktura tabeli dla  `core_users`
 -- 
 
+DROP TABLE IF EXISTS `core_users`;
 CREATE TABLE `core_users` (
   `id_user` bigint(20) unsigned NOT NULL,
   `login` varchar(64) NOT NULL,
@@ -188,7 +188,7 @@ CREATE TABLE `core_users` (
   `date_add` datetime NOT NULL,
   `enabled` tinyint(1) NOT NULL default '1',
   PRIMARY KEY  (`id_user`)
-) TYPE=MyISAM;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- 
 -- Zrzut danych tabeli `core_users`
@@ -201,13 +201,14 @@ CREATE TABLE `core_users` (
 -- Struktura tabeli dla  `core_usersmeta`
 -- 
 
+DROP TABLE IF EXISTS `core_usersmeta`;
 CREATE TABLE `core_usersmeta` (
   `id_meta` bigint(20) unsigned NOT NULL,
   `id_user` bigint(20) unsigned NOT NULL,
   `key` varchar(255) NOT NULL,
   `value` longtext NOT NULL,
   PRIMARY KEY  (`id_meta`)
-) TYPE=MyISAM;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- 
 -- Zrzut danych tabeli `core_usersmeta`
