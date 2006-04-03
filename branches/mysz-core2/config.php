e57a3fa5f87709ea6fac0d81049b76fc737467ef
mysz <mysz@68905588-48f1-0310-a723-09cfc018eb8a>
2006-04-03 19:08:28 +0000

    - db tables: core_usermeta and core_postmeta are removed, in place of them is created one core_meta   and now constant: CORE_POSTMETA and CORE_USERMETA are removed to, and in place of them is created new: CORE_META

--- branches/mysz-core2/config.php
+++ branches/mysz-core2/config.php
@@ -16,10 +16,9 @@ define('TBL_MENU',          TBL_PREFIX . 'menusection');
 define('TBL_P2C',           TBL_PREFIX . 'p2c');
 define('TBL_POSTCATS',      TBL_PREFIX . 'postcats');
 define('TBL_POSTGROUPS',    TBL_PREFIX . 'postgroups');
-define('TBL_POSTMETA',      TBL_PREFIX . 'postmeta');
 define('TBL_POSTS',         TBL_PREFIX . 'posts');
 define('TBL_USERS',         TBL_PREFIX . 'users');
-define('TBL_USERSMETA',     TBL_PREFIX . 'usersmeta');
+define('TBL_META',          TBL_PREFIX . 'meta');
 
 define('TECH_MAIL',         'mysz@localhost');
 
