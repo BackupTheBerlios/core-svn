9db4e3b7e6ab2c2be2d56aaa1bf093999d63b248
mysz <mysz@68905588-48f1-0310-a723-09cfc018eb8a>
2006-06-08 22:04:11 +0000

    - very early state of permissions class

---
+++ branches/mysz-core2/inc/class_perms.php
@@ -0,0 +1,40 @@
+<?php
+
+// vim: expandtab shiftwidth=4 softtabstop=4 tabstop=4
+
+class Perms
+{
+    protected $perms = array(
+        'news'          => array('edit' =>  1, 'add' =>  2, 'del' => 3),
+        'news_cat'      => array('edit' =>  4, 'add' =>  5, 'del' => 6),
+        'pages'         => array('edit' =>  7, 'add' =>  8, 'del' => 9),
+        'comments'      => array('edit' => 12, 'add' => 13, 'del' => 14),
+        'links'         => array('edit' => 15, 'add' => 16, 'del' => 17),
+        'photos'        => array('add'  => 10, 'del' => 11),
+        'config'        => array('edit' => 18),
+        'templates'     => array('edit' => 19),
+        'newsletter'    => array('edit' => 20),
+        'users'         => array('edit' => 21),
+    );
+
+    public function __construct()
+    {
+        $this->db = CoreDB::connect();
+    }
+
+}
+
+/*
+
+zalozenia:
+admin moze zdefiniowac sobie dowolna ilosc poziomow uprawnien
+i pozniej roznym userom przypisywac konkretne poziomy.
+np tworzy poziom 1, i przypisuje do tego poziomu mozliwosc dodawania
+newsow, dodawania i edycji stron, i zarzadzanie newsletterem. Pozniej
+przypisuje do tego poziomu kilku userow, i maja oni wlasnie takie
+uprawnienia. Dobrze by bylo jakby mozna bylo (trzeba?) kazdy z tych
+poziomow odpowiednio nazwac, tak jak admin sobie zazyczy.
+
+*/
+
+?>
